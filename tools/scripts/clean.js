41b255e01bc98204fa85a7cddb7be1670208868c
Benjamin Kniffler <bkniffler@me.com>
2016-11-13 00:07:07 +0100

    no message

--- tools/scripts/clean.js
+++ tools/scripts/clean.js
@@ -4,10 +4,10 @@ const pathResolve = require('path').resolve;
 const appRootPath = require('app-root-dir').get();
 const envVars = require(pathResolve(__dirname, '..', 'config/envVars'));
 const { exec } = require(pathResolve(__dirname, '..', 'utils.js'));
-const rimraf = pathResolve(__dirname, '..', '..', 'node_modules', '.bin', 'rimraf');
+const rimraf = pathResolve(__dirname, '..', '..', '..', 'rimraf');
 
 const buildOutput = pathResolve(appRootPath, envVars.BUNDLE_OUTPUT_PATH);
 
-const cmd = `${rimraf} ${buildOutput}`;
+const cmd = `node ${rimraf} ${buildOutput}`;
 
 exec(cmd);
