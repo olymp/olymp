b1b15ac4cb4a3cf263cb6b105e69334a491906c6
Benjamin Kniffler <bkniffler@me.com>
2016-11-18 12:06:12 +0100

    replace babel-plugin-import, fix versions

--- tools/webpack/configFactory.js
+++ tools/webpack/configFactory.js
@@ -419,7 +419,7 @@ function webpackConfigFactory({ target, mode }, { json }) {
               // would have had to do ourselves to get code splitting w/SSR
               // support working.
               'transform-decorators-legacy',
-              ifClient([path.resolve(__dirname, '..', '..', 'babel-plugin-import'), { libraryName: 'antd', style: 'css' }]),
+              ifClient(['babel-plugin-import', { libraryName: 'antd', style: 'css' }]),
               // @see https://github.com/ctrlplusb/code-split-component
               [
                 'code-split-component/babel',
