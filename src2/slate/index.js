d2ef834fcd97f4cff0aaa0da577f3e39ab24a8b8
Benjamin Kniffler <bkniffler@me.com>
2016-11-22 17:07:38 +0100

    bugfixes, version bumps, ssr cache

--- src2/slate/index.js
+++ src2/slate/index.js
@@ -1,6 +1,3 @@
-import injector from 'react-frame-aware-selection-plugin';
-injector();
-
 export { Block, Editor } from 'slate';
 export { useBlockBase, useBlockResize, useBlockAlign, useBlockToolbar } from './block-decorators';
 export { withState, withSidebar, withToolbar, withAutoMarkdown, withUniqueId, useBlocks } from './editor-decorators';
