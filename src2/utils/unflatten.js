1227d139714393091c1c8251c046af72a103c4b7
Philipp Achenbach <bkniffler@philipp-achenbach.de>
2016-12-04 21:58:58 +0100

    utils gelöscht unflatten aufgeräumt page-Modal: Select durch Tree-Select ersetzt

--- src2/utils/unflatten.js
+++ src2/utils/unflatten.js
@@ -1,20 +1,21 @@
-let sortBy = require('lodash/sortBy');
+const sortBy = require('lodash/sortBy');
 
-const unflatten = (array, options, parentId, level) => {
-  if (!options) options = {};
-  if (!options.parentId) options.parentId = 'parentId';
-  if (!options.id) options.id = 'id';
-  let mapper = options.mapper || (item => item);
+const unflatten = (items, { id = 'id', parentId = 'parentId', mapper = item => item }, parent = null, level = 0) => {
+  let children = items
+    .filter(item => item[parentId] === parent)
+    .map(item => mapper(
+      item,
+      parent ? items.filter(x => x[id] === parent)[0] : null
+    ));
 
-  parentId = typeof parentId !== 'undefined' ? parentId : null;
-  level = typeof level !== 'undefined' ? level : 0;
-
-  let parent = parentId ? array.filter(x => x[options.id] == parentId)[0] : null;
-  let children = array.filter(item => item[options.parentId] == parentId).map(item => mapper(item, parent));
+  // Sortieren nach Order-Nummer
   children = sortBy(children, [item => item.order]);
-  children.forEach(item => {
-    item.children = unflatten(array, options, item[options.id], level + 1);
+
+  // Rekursion
+  children.forEach((item) => {
+    item.children = unflatten(items, { id, parentId, mapper }, item[id], level + 1);
   });
+
   return children;
 };
 
