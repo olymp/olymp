2f0d969c47f9bc01b6f9e7f4f67793116a316339
Benjamin Kniffler <bkniffler@me.com>
2016-12-06 21:06:52 +0000

    moment utc +1

--- src2/locale-de.js
+++ src2/locale-de.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import enUS from 'antd/lib/locale-provider/en_US';
 import LocaleProvider from 'antd/lib/locale-provider';
-import moment from 'moment';
+import momento from 'moment';
 import 'moment/locale/de';
-moment.locale('de');
+momento.locale('de');
 
 export const withLocale = WrappedComponent => props => (
   <LocaleProvider locale={enUS}>
     <WrappedComponent {...props} />
   </LocaleProvider>
 );
+
+export const moment = () => momento().utcOffset('+01:00');
 
 export default props => (
   <LocaleProvider locale={enUS}>
