0f4d28c55b97efdc15e90516392fbf6770fdddd3
Philipp Achenbach <bkniffler@philipp-achenbach.de>
2016-12-09 12:38:01 +0100

    Image-Block und Container-Block

---
+++ src2/cms/blocks/container/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { useGenericBlock, GenericBlock } from 'olymp/cms';
+
+@useGenericBlock({
+  label: 'Container',
+  resize: {
+    coverOnResize: true,
+    width: '100%',
+    stepX: '10%',
+  },
+})
+export default class GZContainer extends Component {
+  render() {
+    return (
+      <GenericBlock {...this.props} style={{ ...this.props.style, position: 'relative', minHeight: 30, overflow: 'hidden' }} />
+    );
+  }
+}
