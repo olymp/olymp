534ab9cf4f9ab08cec30535e463293d2ed293c3e
Philipp Achenbach <github@philipp-achenbach.de>
2016-12-11 02:22:06 +0100

    ImageBlock fertig Image-Picker/Cropper um einige Funktionen erweitert und Fehler behoben

--- src2/cms/blocks/image/index.js
+++ src2/cms/blocks/image/index.js
@@ -1,8 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { useGenericBlock, GenericBlock } from 'olymp/cms';
-import { Image } from 'olymp/cms';
+import { useGenericBlock, GenericBlock, Image } from 'olymp/cms';
 
-const defaultImage = 'whoa.jpg';
 const actions = props => [{
   type: 'image.src',
   icon: 'picture-o',
@@ -18,7 +16,11 @@ const actions = props => [{
   props: ['image'],
   category: 'Media',
   editable: false,
-  resize: { coverOnResize: true },
+  resize: {
+    coverOnResize: true,
+    ratio: true,
+  },
+  align: true,
   actions,
 })
 export default class ImageBlock extends Component {
@@ -27,39 +29,41 @@ export default class ImageBlock extends Component {
     style: PropTypes.object,
     className: PropTypes.string,
     isFocused: PropTypes.bool,
-    attributes: PropTypes.object,
     getData: PropTypes.func,
+    setData: PropTypes.func,
   }
 
   render() {
-    const { setData, getData, editor, className } = this.props;
+    const { setData, getData, className } = this.props;
     const { style, children, ...rest } = this.props;
-    const value = getData('image', { url: defaultImage });
-    const { readOnly } = editor.props;
-    const height = style.height.replace('-', '');
+    const value = getData('image', undefined);
 
     const styles = {
       backgroundColor: 'gray',
       position: 'relative',
       zIndex: 2,
-      ...style
+      ...style,
     };
 
     const innerStyle = {
       display: 'block',
     };
 
+    const width = !value ? styles.width : (value.crop ? value.crop[0] : value.width);
+    const height = !value ? styles.height : (value.crop ? value.crop[1] : value.height);
+
     return (
       <GenericBlock {...rest} style={{ ...styles, height: 'auto' }}>
-        <figure className={className} style={{ margin: 0, ...styles }}>
+        <figure className={className} style={{ margin: 0 }}>
           <Image
             container="div"
             onChange={image => setData({ showMedia: undefined, image })}
+            onCancel={() => setData({ showMedia: false })}
             lightbox
-            onImageClick={readOnly ? ({ showLightbox }) => showLightbox() : () => {}}
-            showMediathek={getData('showMedia')}
-            height={height}
-            width="100%"
+            onImageClick={({ showLightbox }) => showLightbox()}
+            showMediathek={getData('showMedia') /* !getData('showMedia') ? false : (value ? value : true) */}
+            width={styles.width}
+            height={(parseInt(styles.width, 10) / width) * height}
             value={value}
             style={innerStyle}
           />
