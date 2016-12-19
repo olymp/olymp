67c89a6755824e14723b8dd83fc1de92ca3d209c
Philipp Achenbach <github@philipp-achenbach.de>
2016-12-19 09:48:13 +0100

    Neue Version Images/Gallery-Block Fehler in Mediathek => Quellensortierung Wenn Seite leer ist, wird nun auch block = null eingetragen

--- src2/cms/blocks/index.js
+++ src2/cms/blocks/index.js
@@ -2,3 +2,4 @@ export { default as Youtube } from './youtube';
 export { default as GoogleMap } from './gmap';
 export { default as Container } from './container';
 export { default as Image } from './image';
+export { default as Images } from './gallery';
