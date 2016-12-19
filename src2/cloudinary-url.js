8242e2bc823c597666b3c10767f7e20d4165ebe6
Benjamin Kniffler <bkniffler@me.com>
2016-12-19 15:15:51 +0100

    cloudinary url fix maxWidth maxHeight with crop

--- src2/cloudinary-url.js
+++ src2/cloudinary-url.js
@@ -15,27 +15,20 @@ export default (url, { maxWidth, maxHeight, width, height, cropX, cropY, quality
   if (url.indexOf('https://res.cloudinary.com/') !== 0) return url;
   let part = defaultState;
   if (cropX !== undefined && cropY !== undefined) {
-    if (!cropX && !cropY) {
-      // 0,0 => Zentrierter Ausschnitt aus der Mitte
-      part += `,w_${width},h_${height},c_fill`;
-    } else {
-      part += `,x_${cropX},y_${cropY},w_${width},h_${height},c_crop`;
-    }
+    part = `x_${cropX},y_${cropY},w_${width},h_${height},c_crop/${part}`;
   } else if (width && height) {
-    part += `,w_${width},h_${height},c_fit`;
-  } else if (maxWidth && maxHeight) {
-    if (part.length > 0) part += ',';
-    part += `,w_${maxWidth},h_${maxHeight},c_fit`;
+    part += `w_${width},h_${height},c_fit/${part}`;
   }
 
+  if (maxWidth && maxHeight) {
+    part += `,w_${maxWidth},h_${maxHeight},c_fit`;
+  }
   if (quality) {
     part += `,q_${quality}`;
   }
-
   if (blur) {
     part += `,e_blur:${blur}`;
   }
-
   if (part === defaultState) {
     part += ',q_75';
   }
