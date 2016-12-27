5dffa57ef61490416d9caa8ace9abd1f19b245db
Benjamin Kniffler <bkniffler@me.com>
2016-12-27 10:57:53 +0100

    Ease config

--- tools/config/envVars.js
+++ tools/config/envVars.js
@@ -16,11 +16,6 @@ const pathResolve = require('path').resolve;
 const dotenv = require('dotenv');
 const appRootPath = require('app-root-dir').get();
 const envFile = pathResolve(appRootPath, './.env');
-const envWhitelistFile = pathResolve(appRootPath, '.env_whitelist');
-
-const whitelist = fs.readFileSync(envWhitelistFile, 'utf8')
-  .split('\n')
-  .filter(x => x.trim() !== '' && !x.startsWith('#'));
 
 const mergedEnvVars = fs.existsSync(envFile)
   // We have a .env file, which we need to merge with the standard vars.
@@ -37,12 +32,26 @@ const mergedEnvVars = fs.existsSync(envFile)
 // Now we need to filter our mergedEnvVars to only contain environment
 // variables that are deemed allowable by our .env_whitelist file.
 const finalEnvVars = Object.keys(mergedEnvVars)
-  .filter(key => whitelist.find(wkey => wkey === key))
   .reduce((acc, key) => {
     acc[key] = mergedEnvVars[key]; // eslint-disable-line no-param-reassign
     return acc;
   }, {});
 
+if (!finalEnvVars.SERVER_PORT) finalEnvVars.SERVER_PORT = 3000;
+if (!finalEnvVars.CLIENT_DEVSERVER_PORT) finalEnvVars.CLIENT_DEVSERVER_PORT = 3001;
+if (!finalEnvVars.URL) finalEnvVars.URL = `http://localhost:${finalEnvVars.SERVER_PORT}`;
+if (!finalEnvVars.BUNDLE_OUTPUT_PATH) finalEnvVars.BUNDLE_OUTPUT_PATH = './.build';
+if (!finalEnvVars.BUNDLE_ASSETS_FILENAME) finalEnvVars.BUNDLE_ASSETS_FILENAME = 'assets.json';
+if (!finalEnvVars.CLIENT_BUNDLE_HTTP_PATH) finalEnvVars.CLIENT_BUNDLE_HTTP_PATH = '/client/';
+if (!finalEnvVars.CLIENT_BUNDLE_CACHE_MAXAGE) finalEnvVars.CLIENT_BUNDLE_CACHE_MAXAGE = process.env.NODE_ENV === 'production' ? '365d' : 1;
+if (!finalEnvVars.SESSION_SECRET) {
+  if (process.env.NODE_ENV === 'production') throw new Error('Please provide the SESSION_SECRET env variable.');
+  finalEnvVars.SESSION_SECRET = 'keyboard-cat';
+}
+if (!finalEnvVars.AUTH_SECRET) {
+  if (process.env.NODE_ENV === 'production') throw new Error('Please provide the AUTH_SECRET env variable.');
+  finalEnvVars.AUTH_SECRET = 'keyboard-cat';
+}
 // Guard that our some of our expected environment variables exist.
 if (!finalEnvVars.BUNDLE_OUTPUT_PATH || !finalEnvVars.BUNDLE_ASSETS_FILENAME) {
   throw new Error('Some of the critical environment variables are missing, please ensure that you have provided them via the command line or via a .env file.');
