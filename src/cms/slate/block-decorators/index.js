29ddc0e59aed6f13405d2d8f96129dcc575016e3
Benjamin Kniffler <bkniffler@me.com>
2017-01-14 11:47:54 +0000

    Minor cleanup & improvements

--- src/cms/slate/block-decorators/index.js
+++
@@ -1,5 +0,0 @@
-export { default as useBlockBase } from './base';
-export { default as useBlockResize } from './resize';
-export { default as useBlockAlign } from './align';
-export { default as useBlockToolbar } from './toolbar';
-export { useGenericBlock, GenericBlock } from './generic';
