70883f6dc18e1d15b11b02945f9f72a68bfe9c98
Benjamin Kniffler <bkniffler@me.com>
2017-01-14 16:13:19 +0000

    bump

--- dev/index.js
+++ dev/index.js
@@ -12,9 +12,9 @@ module.exports = (config) => {
   config.happypackOutputPath = './.build/happypack';
   config.buildOutputPath = './.build';
   config.clientDevServerPort = process.env.PORT ? (process.env.PORT + 1) : 7331;
-  config.url = process.env.URL || `http://localhost:${process.env.PORT || 1337}`;
-  config.env['URL'] = config.url;
-  config.env['API'] = process.env.API || (config.url + '/graphql');
+  config.url = process.env.SERVER_URL || `http://localhost:${process.env.PORT || 1337}`;
+  config.env['SERVER_URL'] = config.url;
+  config.env['GRAPHQL_URL'] = process.env.GRAPHQL_URL || (config.url + '/graphql');
   if (!config.alias) config.alias = {}
   config.alias['react-router'] = path.resolve(rootPath.get(), 'node_modules', 'react-router-v4-decode-uri');
   config.alias['olymp'] = fs.realpathSync(path.resolve(rootPath.get(), 'node_modules', 'olymp'));
