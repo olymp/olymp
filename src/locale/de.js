2e56cda71ae3ad52ddc0ca37c1a0fa099f70f626
Philipp Achenbach <bkniffler@philipp-achenbach.de>
2017-01-16 17:22:00 +0100

    Fehler mit Sommerzeit gefixt

--- src/locale/de.js
+++ src/locale/de.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import enUS from 'antd/lib/locale-provider/en_US';
 import LocaleProvider from 'antd/lib/locale-provider';
-import momento from 'moment';
+// import momento from 'moment';
+import momentTimezone from 'moment-timezone';
 import 'moment/locale/de';
-momento.locale('de');
+// momento.locale('de');
+momentTimezone.locale('de');
 
 export const withLocale = WrappedComponent => props => (
   <LocaleProvider locale={enUS}>
@@ -11,7 +13,8 @@ export const withLocale = WrappedComponent => props => (
   </LocaleProvider>
 );
 
-export const moment = (x1, x2, x3, x4, x5) => momento(x1, x2, x3, x4, x5).utcOffset('+01:00');
+// export const moment = (x1, x2, x3, x4, x5) => momento(x1, x2, x3, x4, x5).utcOffset('+01:00');
+export const moment = (x1, x2, x3, x4, x5) => momentTimezone(x1, x2, x3, x4, x5).tz('Europe/Berlin');
 
 export default props => (
   <LocaleProvider locale={enUS}>
