032897203683080a4691c52fc6144098f43c2e63
Benjamin Kniffler <bkniffler@me.com>
2017-01-17 15:32:05 +0000

    experimental new sidebar in collection modal

--- src/cms/views/collections-new/list-sidebar.js
+++ src/cms/views/collections-new/list-sidebar.js
@@ -1,7 +1,62 @@
 import React, { Component, PropTypes } from 'react';
 import capitalize from 'lodash/upperFirst';
-import { Table, Menu, Icon, Affix, Dropdown, Button } from 'antd';
+import { Table, Menu, Icon, Affix, Dropdown, Button, Tabs, Input } from 'antd';
 import { withRouter, withCollection, withItems, Link, gql, withApollo } from 'olymp';
+import { createComponent } from 'react-fela';
+function hexToRgba(hex, a) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${a || 1})` : null;
+}
+
+const Sidebar = createComponent(() => ({
+  left: 0,
+  top: 0,
+  width: '340px',
+  bottom: 0,
+  position: 'fixed',
+  backgroundColor: '#f9f9f9',
+  borderRight: '1px solid #e0e0e0',
+  zIndex: 3,
+}));
+
+const Panel = createComponent(({ seperator, align, padding }) => ({
+  padding: padding || '5px 10px',
+  borderTop: seperator ? '1px solid #e6e6e6' : 0,
+  textAlign: align,
+}));
+
+const Title = createComponent(() => ({
+  fontSize: '20px',
+  paddingTop: '10px',
+  paddingBottm: '5px',
+}), 'h1');
+
+const Li = createComponent(() => ({
+  fontSize: '16px',
+  ':hover': {
+    backgroundColor: 'white',
+  },
+}), 'li');
+
+const StyledInput = createComponent(({ theme }) => ({
+  display: 'block',
+  margin: '10px auto',
+  width: '90%',
+  ':hover': {
+    borderColor: `${theme.color}!important`,
+  },
+  ':focus': {
+    borderColor: `${theme.color}!important`,
+    boxShadow: `0 0 0 2px ${hexToRgba(theme.color, 0.2)}!important`,
+  },
+}), props => <Input {...props} />, ['placeholder']);
+
+const StyledButton = createComponent(({ theme }) => ({
+  ':hover': {
+    color: `${theme.color}!important`,
+    borderColor: `${theme.color}!important`,
+  },
+}), props => <Button {...props} />, ['placeholder']);
 
 @withItems()
 @withApollo
@@ -13,23 +68,37 @@ export default class CollectionListSidebar extends Component {
 
   render() {
     const { onClick, collection, name, saveCollectionItem, removeCollectionItem, location, items } = this.props;
+
+    console.log('jadda');
     return (
-      <div>
-        <Menu defaultOpenKeys={['p']} mode="inline" onClick={this.handleClick}>
-          <div className="p-1" style={{ textAlign: 'center' }}>
-            Test
-          </div>
-          <Menu.SubMenu key="p" title="Veröffentlicht">
-            {items && items.map(item =>
-              <Menu.Item key={item.id}>
-                <Link to={{ ...location, query: { ...location.query, [`@${collection.name}`]: item.id } }}>{item.name}</Link>
-              </Menu.Item>)}
-          </Menu.SubMenu>
-          <Menu.SubMenu key="d" title="Entwurf">
-            <Menu.Item key="d0">SubMenuItem</Menu.Item>
-          </Menu.SubMenu>
-        </Menu>
-      </div>
+      <Sidebar>
+        <Panel align="center" padding="10px 10px">
+          <Title>Mediathek</Title>
+          <StyledButton><i className="fa fa-plus" /> Neu erstellen</StyledButton>
+        </Panel>
+        <Panel seperator>
+          <StyledInput placeholder="Suche ..." />
+        </Panel>
+        <Panel seperator padding="0px 10px">
+          <Tabs>
+            {['Öffentlich', 'Entwurf', 'Archiv', 'Papierkorb'].map(name =>
+              <Tabs.TabPane tab={name} key={name}>
+                <Panel>
+                  <ul>
+                    {items && items.map(item => (
+                      <Li key={item.id}>
+                        <Link to={{ ...location, query: { ...location.query, [`@${collection.name}`]: item.id } }}>
+                          {item.kurz || item.name}
+                        </Link>
+                      </Li>
+                    ))}
+                  </ul>
+                </Panel>
+              </Tabs.TabPane>
+            )}
+          </Tabs>
+        </Panel>
+      </Sidebar>
     );
   }
 }
