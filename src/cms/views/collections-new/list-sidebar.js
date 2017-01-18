a85d7e87a8402979741f5af8dd276568536e428b
Philipp Achenbach <bkniffler@philipp-achenbach.de>
2017-01-18 20:17:46 +0100

    Sidebar aufgeräumt

--- src/cms/views/collections-new/list-sidebar.js
+++ src/cms/views/collections-new/list-sidebar.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import { Dropdown, Button, Tabs, Input, Card, Menu, Icon, Table, Col, Spin } from 'antd';
+import { Dropdown, Button, Tabs, Input, Card, Menu, Icon, Col, Spin } from 'antd';
 import { withRouter, withCollection, withItems, Link, withApollo, throttleInput } from 'olymp';
 import Image from '../../edits/image';
 import { createComponent } from 'react-fela';
 import tinycolor from 'tinycolor2';
 import Filter from './filter';
 
-function hexToRgba(hex, a) {
-  // const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  // return result ? `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${a || 1})` : null;
-  return tinycolor(hex).setAlpha(a).toRgbString();
-}
-
-const Sidebar = createComponent(() => ({
+const StyledSidebar = createComponent(() => ({
   left: 0,
   top: 0,
   width: '340px',
@@ -23,42 +17,17 @@ const Sidebar = createComponent(() => ({
   zIndex: 3,
 }));
 
-const Panel = createComponent(({ seperator, align, padding }) => ({
+const StyledPanel = createComponent(({ seperator, align, padding }) => ({
   padding: padding || '5px 10px',
   borderTop: seperator ? '1px solid #e6e6e6' : 0,
   textAlign: align,
 }));
 
-const Title = createComponent(() => ({
+const StyledTitle = createComponent(() => ({
   fontSize: '20px',
   paddingTop: '10px',
   paddingBottm: '5px',
 }), 'h1');
-
-const Li = createComponent(({ active }) => ({
-  fontWeight: active && 'bold',
-  fontSize: '16px',
-  ':hover': {
-    backgroundColor: 'white',
-  },
-}), 'li');
-
-const StyledLink = createComponent(({ active }) => ({
-  fontWeight: active && 'bold',
-}), props => <Link {...props} />, ['to']);
-
-const StyledInput = createComponent(({ theme }) => ({
-  display: 'block',
-  margin: '10px auto',
-  width: '90%',
-  ':hover': {
-    borderColor: `${theme.color}!important`,
-  },
-  ':focus': {
-    borderColor: `${theme.color}!important`,
-    boxShadow: `0 0 0 2px ${hexToRgba(theme.color, 0.2)}!important`,
-  },
-}), props => <Input {...props} />, ['placeholder']);
 
 const StyledButton = createComponent(({ theme }) => ({
   ':hover': {
@@ -67,11 +36,65 @@ const StyledButton = createComponent(({ theme }) => ({
   },
 }), props => <Button {...props} />, ['placeholder']);
 
+const StyledCard = createComponent(({ isActive, color }) => {
+  const colorStyle = {};
+  if (color) {
+    colorStyle.backgroundColor = tinycolor(color).setAlpha(0.33).toRgbString();
+    // colorStyle.color = tinycolor(item.farbe).isLight ? '#333' : '#CCC';
+    colorStyle.color = '#333';
+  }
+
+  const activeStyle = {};
+  if (isActive) {
+    activeStyle.left = '15px';
+    activeStyle.borderWidth = '0!important';
+    activeStyle.boxShadow = '0 0 5px 0 rgba(0, 0, 0, .33)';
+  }
+
+  return {
+    cursor: 'pointer',
+    margin: '3px 0',
+    ...activeStyle,
+    ...colorStyle,
+    '> .ant-card-extra': {
+      top: '5px',
+      right: '5px',
+    },
+    '> .ant-card-body': {
+      padding: 0,
+      ...colorStyle,
+    },
+  };
+}, props => <Card {...props} />, ['onClick', 'extra']);
+
+const StyledCardContent = createComponent(() => ({
+  padding: '5px',
+  position: 'absolute',
+  left: '65px',
+  top: '50%',
+  transform: 'translateY(-50%)',
+}));
+
+const StyledCardTitle = createComponent(() => ({
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  width: '200px',
+  whiteSpace: 'nowrap',
+}), 'h6');
+
+const StyledCardParagraph = createComponent(() => ({
+  margin: 0,
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  width: '200px',
+  whiteSpace: 'nowrap',
+}), 'p');
+
 @withItems({ })
 @withRouter
 class CollectionList extends Component {
   getLink = (item) => {
-    const { onClick, collection, name, saveCollectionItem, removeCollectionItem, location, items } = this.props;
+    const { collection, location } = this.props;
     const { pathname } = location;
     return { pathname, query: { ...location.query, [`@${collection.name.toLowerCase()}`]: item ? item.id : null } };
   }
@@ -100,43 +123,20 @@ class CollectionList extends Component {
   renderCard = (item) => {
     const { id, router } = this.props;
 
-    const colorStyle = {};
-    if (item.farbe) {
-      colorStyle.backgroundColor = tinycolor(item.farbe).setAlpha(0.33).toRgbString();
-      // colorStyle.color = tinycolor(item.farbe).isLight ? '#333' : '#CCC';
-      colorStyle.color = '#333';
-    }
-
-    const activeStyle = {};
-    if (item.id === id) {
-      activeStyle.left = 15;
-      activeStyle.border = 'none';
-      activeStyle.boxShadow = '0 0 5px 0 rgba(0, 0, 0, .33)';
-      activeStyle.margin = '3px 0';
-    }
-
     return (
-      <Card
+      <StyledCard
         onClick={() => router.push(this.getLink(item))}
         key={item.id}
         extra={<Dropdown overlay={this.renderMenu()}><Icon type="edit" /></Dropdown>}
-        style={{ cursor: 'pointer', margin: '3px 0', ...colorStyle, ...activeStyle }}
-        bodyStyle={{ padding: 0, ...colorStyle }}
+        isActive={item.id === id}
+        color={item.farbe}
       >
-        <div style={{ width: 60 }}>
-          {
-            item.bild ? (
-              <Image value={item.bild} width={60} ratio={1} />
-            ) : (
-              <div style={{ width: 60, height: 60 }} />
-            )
-          }
-        </div>
-        <div style={{ padding: 5, position: 'absolute', left: 65, top: '50%', transform: 'translateY(-50%)' }}>
-          <h6 style={{ textOverflow: 'ellipsis', overflow: 'hidden', width: 200, whiteSpace: 'nowrap' }}>{item.kurz || item.name || 'Kein Titel'}</h6>
-          <p style={{ margin: 0, textOverflow: 'ellipsis', overflow: 'hidden', width: 200, whiteSpace: 'nowrap' }}>weitere Infos</p>
-        </div>
-      </Card>
+        {item.bild && <Image value={item.bild} width={60} ratio={1} />}
+        <StyledCardContent>
+          <StyledCardTitle>{item.kurz || item.name || 'Kein Titel'}</StyledCardTitle>
+          <StyledCardParagraph>weitere Infos</StyledCardParagraph>
+        </StyledCardContent>
+      </StyledCard>
     );
   }
 
@@ -160,10 +160,9 @@ const states = {
   REMOVED: 'Papierkorb',
 };
 
-@withItems({ state: Object.keys(states) })
 @withApollo
 @withRouter
-// @withCollection
+@withCollection
 export default class CollectionListSidebar extends Component {
   state = { query: { state: { eq: 'PUBLISHED' } }, filtering: false, searchText: '' };
   throttle = throttleInput();
@@ -210,33 +209,33 @@ export default class CollectionListSidebar extends Component {
     const { query, filtering, searchText } = this.state;
 
     return (
-      <Sidebar>
-        <Panel align="center" padding="10px 10px">
-          <Title>{collection.name}</Title>
+      <StyledSidebar>
+        <StyledPanel align="center" padding="10px 10px">
+          <StyledTitle>{collection.name}</StyledTitle>
           <StyledButton><Link to={this.getLink()}><i className="fa fa-plus" /> Neu erstellen</Link></StyledButton>
-        </Panel>
-        <Panel seperator>
+        </StyledPanel>
+        <StyledPanel seperator>
           <Input.Group size="large">
             <Col span="18">
               <Input placeholder="Suche ..." onChange={this.search} value={searchText} />
             </Col>
-            <Col span="4">
-              {!filtering && <Filter onFilter={this.setQuery} collection={collection} />}
-              {filtering && <Button onClick={e => this.setQueryToState()}>Reset <i className="fa fa-clear" /></Button>}
+            <Col span="6" className="pr-0">
+              {!filtering && <Filter onFilter={this.setQuery} collection={collection} style={{ width: '100%' }} />}
+              {filtering && <Button onClick={e => this.setQueryToState()} style={{ width: '100%' }}>Reset</Button>}
             </Col>
           </Input.Group>
-        </Panel>
+        </StyledPanel>
         {!filtering && (
-          <Panel seperator>
+          <StyledPanel seperator>
             <Tabs onChange={eq => this.setQueryToState(eq)} style={{ marginBottom: -16 }}>
               {Object.keys(states).map(name => <Tabs.TabPane tab={states[name]} key={name} />)}
             </Tabs>
-          </Panel>
+          </StyledPanel>
         )}
-        <Panel>
+        <StyledPanel>
           <CollectionList collection={collection} location={location} id={id} query={query} />
-        </Panel>
-      </Sidebar>
+        </StyledPanel>
+      </StyledSidebar>
     );
   }
 }
