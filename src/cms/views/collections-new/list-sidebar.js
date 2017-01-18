d2ee1fda899657aea666c75c757858547462d764
Benjamin Kniffler <bkniffler@me.com>
2017-01-18 15:37:54 +0000

    clean

--- src/cms/views/collections-new/list-sidebar.js
+++ src/cms/views/collections-new/list-sidebar.js
@@ -4,7 +4,6 @@ import { withRouter, withCollection, withItems, Link, gql, withApollo, throttleI
 import { createComponent } from 'react-fela';
 import Filter from './filter';
 
-
 function hexToRgba(hex, a) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${a || 1})` : null;
