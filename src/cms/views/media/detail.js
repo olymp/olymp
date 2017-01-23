b8edcd45509cec6a9b7397465c04c9f46b8d69b8
Philipp Achenbach <bkniffler@philipp-achenbach.de>
2017-01-23 14:56:42 +0100

    Close-Button in media! ;)

--- src/cms/views/media/detail.js
+++ src/cms/views/media/detail.js
@@ -6,8 +6,80 @@ import Image from '../../edits/image';
 
 const FormItemLayout = { labelCol: { span: 8 }, wrapperCol: { span: 16 }, style: { marginBottom: '0' } };
 
+const MediaForm = Form.create()(
+  (props) => {
+    const { item, form, save, remove, onClose } = props;
+    const { getFieldDecorator } = form;
+
+    if (!item) {
+      return <Spin style={{ minHeight: 400 }} />;
+    }
+
+    return (
+      <div>
+        <Form.Item key="id" label="ID" {...FormItemLayout}>
+          {getFieldDecorator('id', {
+            initialValue: item.id,
+          })(
+            <Input disabled placeholder="ID" />
+          )}
+        </Form.Item>
+        <Form.Item key="caption" label="Bezeichnung" {...FormItemLayout}>
+          {getFieldDecorator('caption', {
+            initialValue: item.caption,
+          })(
+            <Input placeholder="Bezeichnung" />
+          )}
+        </Form.Item>
+        <Form.Item key="source" label="Quelle" {...FormItemLayout}>
+          {getFieldDecorator('source', {
+            initialValue: item.source,
+          })(
+            <Input placeholder="Quelle" />
+          )}
+        </Form.Item>
+        <Form.Item key="tags" label="Tags" {...FormItemLayout}>
+          {getFieldDecorator('tags', {
+            initialValue: item.tags || [],
+          })(
+            <Select {...props} tags searchPlaceholder="Suche ..." />
+          )}
+        </Form.Item>
+
+        <Image value={{ ...item }} width="100%" readOnly className="my-1" />
+
+        <Form.Item key="size" label="Größe" {...FormItemLayout}>
+          <Input disabled placeholder="Größe" defaultValue={`${item.width}x${item.height}`} />
+        </Form.Item>
+        <Form.Item key="date" label="Hinzugefügt" {...FormItemLayout}>
+          <Input disabled placeholder="Hinzugefügt" defaultValue={`${moment(item.createdAt).format('DD. MMMM YYYY, HH:mm:ss')} Uhr`} />
+        </Form.Item>
+        <Form.Item key="format" label="Format" {...FormItemLayout}>
+          <Input disabled placeholder="Format" defaultValue={item.format} />
+        </Form.Item>
+        { item.format === 'pdf' ? (
+          <Form.Item key="pages" label="Seiten" {...FormItemLayout}>
+            <Input disabled placeholder="Seiten" defaultValue={item.pages} />
+          </Form.Item>
+        ) : undefined }
+        <Form.Item key="bytes" label="Dateigröße" {...FormItemLayout}>
+          <Input disabled placeholder="Dateigröße" defaultValue={`${item.bytes / 1000} kB`} />
+        </Form.Item>
+
+        <div style={{ float: 'right' }}>
+          <Button onClick={save} type="primary">Speichern</Button>&nbsp;
+          <Button onClick={remove}>Löschen</Button>&nbsp;
+          <Button onClick={onClose}>Abbrechen</Button>
+        </div>
+
+        <div style={{ clear: 'both' }} />
+      </div>
+    );
+  }
+);
+
 @withFile
-export default class MediaModal extends Component {
+export default class MediaDetail extends Component {
   handleCancel = () => {
     this.props.onClose();
   };
@@ -36,68 +108,14 @@ export default class MediaModal extends Component {
   };
 
   render() {
-    return Form.create()(
-      (props) => {
-        const { item, form } = props;
-        const { getFieldDecorator } = form;
-
-        if (!item) {
-          return <Spin style={{ minHeight: 400 }} />;
-        }
-
-        return (
-          <div>
-            <Form.Item key="id" label="ID" {...FormItemLayout}>
-              {getFieldDecorator('id', {
-                initialValue: item.id,
-              })(
-                <Input disabled placeholder="ID" />
-              )}
-            </Form.Item>
-            <Form.Item key="caption" label="Bezeichnung" {...FormItemLayout}>
-              {getFieldDecorator('caption', {
-                initialValue: item.caption,
-              })(
-                <Input placeholder="Bezeichnung" />
-              )}
-            </Form.Item>
-            <Form.Item key="source" label="Quelle" {...FormItemLayout}>
-              {getFieldDecorator('source', {
-                initialValue: item.source,
-              })(
-                <Input placeholder="Quelle" />
-              )}
-            </Form.Item>
-            <Form.Item key="tags" label="Tags" {...FormItemLayout}>
-              {getFieldDecorator('tags', {
-                initialValue: item.tags || [],
-              })(
-                <Select {...props} tags searchPlaceholder="Suche ..." />
-              )}
-            </Form.Item>
-
-            <Image value={{ ...item }} width="100%" readOnly />
-
-            <Form.Item key="size" label="Größe" {...FormItemLayout} style={{ marginTop: '24px', marginBottom: '0' }}>
-              <Input disabled placeholder="Größe" defaultValue={`${item.width}x${item.height}`} />
-            </Form.Item>
-            <Form.Item key="date" label="Hinzugefügt" {...FormItemLayout}>
-              <Input disabled placeholder="Hinzugefügt" defaultValue={`${moment(item.createdAt).format('DD. MMMM YYYY, HH:mm:ss')} Uhr`} />
-            </Form.Item>
-            <Form.Item key="format" label="Format" {...FormItemLayout}>
-              <Input disabled placeholder="Format" defaultValue={item.format} />
-            </Form.Item>
-            { item.format === 'pdf' ? (
-              <Form.Item key="pages" label="Seiten" {...FormItemLayout}>
-                <Input disabled placeholder="Seiten" defaultValue={item.pages} />
-              </Form.Item>
-            ) : undefined }
-            <Form.Item key="bytes" label="Dateigröße" {...FormItemLayout}>
-              <Input disabled placeholder="Dateigröße" defaultValue={`${item.bytes / 1000} kB`} />
-            </Form.Item>
-          </div>
-        );
-      }
-    );
+    return (
+      <MediaForm
+        {...this.props}
+        ref={form => this.form = form}
+        save={this.handleCreate}
+        remove={this.handleDelete}
+        onClose={this.handleCancel}
+      />
+    )
   }
 }
