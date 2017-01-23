a180e390c57417e5f3fac8450f170114e0f597b6
Benjamin Kniffler <bkniffler@me.com>
2017-01-23 12:19:32 +0100

    gzk

--- src/gzk/blocks/card/index.js
+++ src/gzk/blocks/card/index.js
@@ -3,3 +3,4 @@ export { default as GZCardEinrichtung } from './einrichtung';
 export { default as GZCardTermin } from './termin';
 export { default as GZCard } from './text';
 export { default as GZCardYoutube } from './youtube';
+export { default as GZCardImage } from './image';
--- src/gzk/components/index.js
+++ src/gzk/components/index.js
@@ -1,3 +1,4 @@
 export { default as Accordion } from './accordion2';
+export { default as Carousel } from './carousel';
 export { default as Layout } from './layout';
 export { default as Logo } from './logo';
