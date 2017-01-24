561d8ec9b16840e601b70efd68fe2cb32ac2fc90
Benjamin Kniffler <bkniffler@me.com>
2017-01-24 10:32:54 +0100

    Add toolbelt und css for ul/li rows

---
+++ src/core/toolbelt.js
@@ -0,0 +1,36 @@
+import lget from 'lodash/get';
+import lset from 'lodash/set';
+import ltoString from 'lodash/toString';
+import ltoArray from 'lodash/toArray';
+import ltoObject from 'lodash/toPlainObject';
+import lupperFirst from 'lodash/upperFirst';
+import llowerCase from 'lodash/lowerCase';
+import lsortBy from 'lodash/sortBy';
+import lgroupBy from 'lodash/groupBy';
+import ltake from 'lodash/take';
+import lisEqual from 'lodash/isEqual';
+import lintersection from 'lodash/intersection';
+
+export const toString = (arg) => {
+  if (arg && typeof arg === 'object') return arg.name || arg.id;
+  return ltoString(arg);
+}
+
+export const toObject = (arg) => {
+  return ltoObject(arg);
+}
+
+export const toArray = (arg) => {
+  return ltoArray(arg);
+}
+
+export const intersection = lintersection;
+export const get = lget;
+export const set = lset;
+export const upperFirst = lupperFirst;
+export const lowerCase = llowerCase;
+export const sortBy = lsortBy;
+export const groupBy = lgroupBy;
+export const isEqual = lisEqual;
+export const take = ltake;
+export const addOrRemove = (arr = [], item, shouldAdd) => shouldPush ? [...arr, item] : arr.filter(x => x !== item);
