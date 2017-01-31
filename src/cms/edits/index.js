4729651976f4374f4cd11a86b4b674cd47664f26
Benjamin Kniffler <bkniffler@me.com>
2017-01-31 16:30:24 +0100

    tag select edit & color edit

--- src/cms/edits/index.js
+++ src/cms/edits/index.js
@@ -1,4 +1,5 @@
 export { default as Image } from './image';
 export { default as ColorEditor } from './color';
+export { default as TagSelect } from './tag-select';
 
 export { default as resolveFieldValue } from './resolve-field-value';
