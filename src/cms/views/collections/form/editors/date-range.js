dbb3999ee21969bc06d40d88ad1316c53c9c13a8
Benjamin Kniffler <bkniffler@me.com>
2017-01-31 22:07:13 +0100

    fix

--- src/cms/views/collections/form/editors/date-range.js
+++ src/cms/views/collections/form/editors/date-range.js
@@ -6,7 +6,8 @@ const { RangePicker } = DatePicker;
 export default class DatePickerInt extends Component {
   onChange = (e) => {
     const { onChange } = this.props;
-    const value = (e || []).map(e => parseInt(e.format('x')));
+    const value = e ? e.map(e => parseInt(e.format('x'))) : [];
+    while (value.length < 2) value.push(null);
     onChange(value);
   }
   getValue = () => {
@@ -14,7 +15,7 @@ export default class DatePickerInt extends Component {
     // 2014-09-14T10:32:27.831Z
     return (value || []).map((value) => {
       if (typeof value === 'string') value = parseInt(moment(value.replace(/"/g, '')).format('x'));
-      return value ? moment(value) : undefined;
+      return value ? moment(value) : null;
     });
   }
   render() {
