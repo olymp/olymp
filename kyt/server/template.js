d4a01df3c623ed236edf705fbd1f33bc8d00075b
Philipp Achenbach <github@philipp-achenbach.de>
2017-02-12 19:31:26 +0100

    olymp build and start (not ready and noct published!)

--- kyt/server/template.js
+++ kyt/server/template.js
@@ -1,21 +1,22 @@
 /* eslint-disable prefer-template, max-len */
 
-export default vo => `
+export default ({ helmet, cssBundle, cssMarkup, jsBundle, root }) => `
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta charSet='utf-8' />
-    <meta httpEquiv="Content-Language" content="en" />
+    <meta charSet="utf-8" />
+    <meta httpEquiv="Content-Language" content="de" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-    ${vo.cssBundle ? '<link rel="stylesheet" type="text/css" href="' + vo.cssBundle + '">' : ''}
-    <style id="css-markup">${vo.cssMarkup || ''}</style>
+    ${helmet.title.toString()}
+    ${helmet.meta.toString()}
+    ${helmet.link.toString()}
+    ${cssBundle ? '<link rel="stylesheet" type="text/css" href="' + cssBundle + '">' : ''}
+    <style id="css-markup">${cssMarkup || ''}</style>
   </head>
   <body>
-    <div id="app"><div>${vo.root}</div></div>
-    <script src="${vo.jsBundle}"></script>
+    <div id="app"><div>${root}</div></div>
+    <script src="${jsBundle}"></script>
   </body>
 </html>
 `;
