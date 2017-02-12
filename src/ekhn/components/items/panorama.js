a94a9272ccaa1718a3873b0601ea59d101a62ece
Philipp Achenbach <github@philipp-achenbach.de>
2017-02-12 17:46:47 +0100

    EKHN: items

--- src/ekhn/components/items/panorama.js
+++ src/ekhn/components/items/panorama.js
@@ -14,24 +14,13 @@ export default useItemEdit()(({ children, className, id, header, subheader, text
     ratio = height / width;
   }
 
-  let imageStyle = { width: '100%' };
-  let textStyle = {};
-  let slateStyle = {};
-  if (ratio > 1) {
-    imageStyle = { float: 'right', width: '33%' };
-    textStyle = { float: 'left', width: 'calc(67% - 1rem)', margin: 0 };
-    slateStyle = { columnCount: 1 };
-  } else {
-    ratio = ratio > 0.66 ? 0.66 : ratio;
-  }
-
   return (
     <div key={id}>
       <div className={cn(className, 'item panorama')}>
         {children}
 
-        { bild && bild.url ? (
-          <Image ratio={ratio} value={bild} lightbox cloudinary={{ maxWidth: 1000 }} style={imageStyle}>
+        {bild && bild.url && ratio <= 1 ? (
+          <Image ratio={ratio > 0.66 ? 0.66 : ratio} value={bild} lightbox cloudinary={{ maxWidth: 1000 }} style={{ width: '100%' }}>
             {bild.caption || bild.source ? (
               <div>{bild.caption}<span style={{ float: 'right' }}>{bild.source}</span></div>
             ) : undefined}
@@ -39,16 +28,24 @@ export default useItemEdit()(({ children, className, id, header, subheader, text
         ) : undefined}
 
         {(!!header || !!text || !!tags) && (
-          <div className={`text ${leading ? 'colored' : ''}`} style={textStyle}>
+          <div className={`text ${leading ? 'colored' : ''}`}>
             <h2>{header}</h2>
             {subheader ? <div className="subheader">{subheader}</div> : null}
 
-            {text ? <SlateMateFrontend key={id} value={text} className="slate" style={slateStyle} readOnly /> : null}
+            {text ? <SlateMateFrontend key={id} value={text} className="slate" readOnly /> : null}
 
             {(!query || !query[capitalize(identifier)]) && more ? (
-              <p style={{ marginBottom: '2rem' }}>
+              <p>
                 <Link to={{ pathname, query: { ...query, [capitalize(identifier)]: id } }}>
                   <i className="fa fa-angle-double-right" /> {more}
+                </Link>
+              </p>
+            ) : undefined}
+
+            {query && query[capitalize(identifier)] ? (
+              <p>
+                <Link to={{ pathname, query: { ...query, [capitalize(identifier)]: null } }}>
+                  <i className="fa fa-angle-double-left" /> Zurück zur Übersicht
                 </Link>
               </p>
             ) : undefined}
@@ -60,12 +57,12 @@ export default useItemEdit()(({ children, className, id, header, subheader, text
         <div style={{ clear: 'both' }} />
       </div>
 
-      {query && query[capitalize(identifier)] ? (
-        <p>
-          <Link to={{ pathname, query: { ...query, [capitalize(identifier)]: null } }}>
-            <i className="fa fa-angle-double-left" /> Zurück zur Übersicht
-          </Link>
-        </p>
+      {bild && bild.url && ratio > 1 ? (
+        <Image value={bild} lightbox cloudinary={{ maxWidth: 1000 }} style={{ width: '100%', marginTop: '1rem' }}>
+          {bild.caption || bild.source ? (
+            <div>{bild.caption}<span style={{ float: 'right' }}>{bild.source}</span></div>
+          ) : undefined}
+        </Image>
       ) : undefined}
     </div>
   );
