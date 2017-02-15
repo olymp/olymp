07d0a1a0497ce356f1c9b5092add17aa83f618c6
Philipp Achenbach <github@philipp-achenbach.de>
2017-02-15 16:52:28 +0100

    CmsHelmet => CmsContainer

--- src/cms/index.js
+++ src/cms/index.js
@@ -3,6 +3,5 @@ export { default as CmsContainer } from './views/container';
 export { default as MediaModal } from './views/media/view';
 export { SortableContainer as sortableContainer, SortableElement as sortableElement, SortableHandle as sortableHandle, arrayMove } from 'react-sortable-hoc';
 export { default as Navigation } from './components/navigation';
-export { default as CmsHelmet } from './helmet';
 export * from './decorators';
 export * from './edits';
