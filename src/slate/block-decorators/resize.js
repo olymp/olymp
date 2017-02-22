bb7a441cd7210edad3a0b35a21f7248ff4a74692
Philipp Achenbach <github@philipp-achenbach.de>
2017-02-22 16:57:37 +0100

    remove display inline-block… again

--- src/slate/block-decorators/resize.js
+++ src/slate/block-decorators/resize.js
@@ -87,7 +87,7 @@ export default (options = {}) => Block => {
 
       const blockStyle = {
         ...style,
-        display: 'inline-block'
+        // display: 'inline-block'
       };
       if (height) blockStyle.height = `${height}px`;
 
