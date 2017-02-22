f8e87d7af2fdd702b6d0e5181c21aa331fe543f8
Benjamin Kniffler <bkniffler@me.com>
2017-02-22 18:23:50 +0100

    WithItemNew + page.js withItemNew

--- src/cms/views/pages/page.js
+++ src/cms/views/pages/page.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { Gateway } from 'react-gateway';
-import { Anchor as AnchorCreator, Spin } from 'antd';
-import { withAuth, withItem, Helmet } from 'olymp';
+import { Anchor as AnchorCreator, Spin, Menu, Icon, Dropdown, Button } from 'antd';
+import { withAuth, withItemNew, Helmet, Link } from 'olymp';
 import { SlateMate } from 'olymp/slate';
 import AnchorHelper from 'antd/lib/anchor/anchorHelper';
-import { PageAction } from '../../components';
 
 const Anchor = AnchorCreator.Link;
 const fieldNames = 'id, slug, order, name, parentId, blocks, headings { id, slug, text, children { id, slug, text } }';
 
 @withAuth
-@withItem({ typeName: 'page', fieldNames })
+@withItemNew({ typeName: 'Page', fieldNames })
 export default class CmsPage extends Component {
   constructor() {
     super();
@@ -30,7 +29,7 @@ export default class CmsPage extends Component {
   )
 
   render() {
-    const { auth, item, patch, getReadOnly } = this.props;
+    const { auth, item, patch, getReadOnly, save } = this.props;
 
     if (!item) return <Spin size="large" />;
 
@@ -53,7 +52,33 @@ export default class CmsPage extends Component {
         <SlateMate className="frontend-editor" showUndo readOnly={readOnly} value={item.blocks || null} onChangeHeadings={headings => patch({ headings })} onChange={blocks => patch({ blocks })} />
 
         <Gateway into="action">
-          <PageAction id={item.id} />
+          <Dropdown
+            overlay={(
+              <Menu>
+                <Menu.Item key="page:1">
+                  <a href="javascript:;" onClick={save}>
+                    Speichern
+                  </a>
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item key="page:visitor" disabled>{false ? <Icon type="check" /> : null}Besucher-Modus</Menu.Item>
+                <Menu.Item key="page:settings">
+                  <Link to={{ ...location, query: { '@page': item.id } }}>
+                    Einstellungen
+                  </Link>
+                </Menu.Item>
+                <Menu.Item key="page:delete" disabled>
+                  Seite löschen
+                </Menu.Item>
+              </Menu>
+            )}
+            overlayClassName="ant-dropdown-left"
+            placement="bottomLeft"
+          >
+            <Button shape="circle" size="large">
+              <Icon type="file" />
+            </Button>
+          </Dropdown>
         </Gateway>
       </div>
     );
