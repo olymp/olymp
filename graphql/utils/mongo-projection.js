609659826c805c553f9d540d3e9ebe888b31b5e7
Benjamin Kniffler <bkniffler@me.com>
2017-03-10 16:58:46 +0000

    Projection cleanup

--- graphql/utils/mongo-projection.js
+++ graphql/utils/mongo-projection.js
@@ -1,17 +1,9 @@
-const getProjection2 = (info, context = (info.fieldASTs || info.fieldNodes)[0]) => {
-  return context.selectionSet.selections.reduce((projections, selection) => {
-    projections[selection.name.value] = 1;
-    projections[`${selection.name.value}Id`] = 1;
-    projections[`${selection.name.value}Ids`] = 1;
-    return projections;
-  }, {});
-};
-
-const getProjection = (info, field, context = (info.fieldASTs || info.fieldNodes)[0]) => {
+module.exports = (info, field) => {
+  let context = (info.fieldASTs || info.fieldNodes)[0];
   if (field) {
     context = context.selectionSet.selections.find(x => x.name.value === field);
-    if (!context) return {};
   }
+  if (!context) return {};
   return context.selectionSet.selections.reduce((projections, selection) => {
     projections[selection.name.value] = 1;
     projections[`${selection.name.value}Id`] = 1;
@@ -19,5 +11,3 @@ const getProjection = (info, field, context = (info.fieldASTs || info.fieldNodes
     return projections;
   }, {});
 };
-
-module.exports = getProjection;
