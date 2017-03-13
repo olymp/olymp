767e6db0c25d4f194f97f350b5f6b63abda94d97
Benjamin Kniffler <bkniffler@me.com>
2017-03-13 01:49:33 +0000

    md

--- src/md/index.js
+++ src/md/index.js
@@ -1,54 +1,32 @@
+import all from 'mdast-util-to-hast/lib/all';
 import React, { Component } from 'react';
 import remark from 'remark';
-import reactRenderer from 'remark-react';
-import visit from 'unist-util-visit';
-import all from 'mdast-util-to-hast/lib/all';
+import reactRenderer from './compiler';
+import plugin from './container';
 
-function plugin() {
-  var Parser = this.Parser;
-  var tokenizers = Parser.prototype.inlineTokenizers;
-  var methods = Parser.prototype.inlineMethods;
-  tokenizers.customComponents = tokenizeMention;
-  methods.splice(methods.indexOf('text'), 0, 'customComponents');
-}
-const tryParseJson = (str) => {
-  try {
-    console.log(str.indexOf('[') !== 0 ? `[${str}]` : str, JSON.parse(str.indexOf('[') !== 0 ? `[${str}]` : str));
-    return JSON.parse(str.indexOf('[') !== 0 ? `[${str}]` : str);
-  } catch(ex) {
-    return undefined;
-  }
-}
-function tokenizeMention(eat, value, silent) {
-  var match = /^@(\w+)(\(([^)]+)\))?/.exec(value);
-  if (match) {
-    if (silent) return true;
-    return eat(match[0])({
-      type: 'component',
-      name: match[1],
-      args: match[3] ? tryParseJson(match[3]) : undefined,
-      'children': [{
-        'type': 'text',
-        'value': match[0]
-      }],
-    });
-  }
+const defaultComponents = {
+  text: ({ value, ...props }) => <span {...props}>{value}</span>,
+  heading1: props => <h1 {...props} />,
+  heading2: props => <h2 {...props} />,
+  heading3: props => <h3 {...props} />,
+  heading4: props => <h4 {...props} />,
+  heading5: props => <h5 {...props} />,
+  heading6: props => <h6 {...props} />,
 };
-tokenizeMention.notInLink = true;
-tokenizeMention.locator = locateMention;
-function locateMention(value, fromIndex) {
-  return value.indexOf('@', fromIndex);
-}
 
-export default (components) => remark().use(plugin).use(reactRenderer, {
-  sanitize: false,
-  remarkReactComponents: components,
-  toHast: {
-    handlers: {
-      component: (h, node) => {
-        var props = { name: node.name, args: node.args };
-        return h(node, components[node.name] ? node.name : 'Default', props, all(h, node));
+export default (components) => {
+  const allComponents = { ...defaultComponents, ...components };
+  return remark().use(plugin, { components: allComponents }).use(reactRenderer, {
+    // sanitize: false,
+    remarkReactComponents: allComponents,
+    toHast: {
+      handlers: {
+        react: (h, node) => {
+          const { children, position, ...props } = node;
+          const copy = JSON.parse(JSON.stringify(node));
+          return h(node, allComponents[node.tag] ? node.tag : 'default', props, all(h, copy));
+        }
       }
     }
-  }
-});
+  });
+};
