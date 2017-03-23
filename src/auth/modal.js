4ec6155b6fea543b5317456f22386dfc7d14909a
Philipp Achenbach <philippachenbach@philipps-mbp.localdomain>
2017-03-23 18:13:06 +0100

    Neuer Login-Screen

--- src/auth/modal.js
+++ src/auth/modal.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { styled } from 'olymp';
 import { Modal } from 'antd';
+import tinycolor from 'tinycolor2';
 
-const style = {
-  backgroundColor: 'whitesmoke',
-  background: 'linear-gradient(0deg,#002673,#004e93)',
-  display: 'flex',
-  '> .ant-modal': {
-    top: 0,
-    margin: 'auto',
-    paddingBottom: 24,
-    paddingTop: 24,
-    '> .ant-modal-content': {
-      borderRadius: 0,
+const style = theme => {
+  const color = theme.color || '#3271A8';
+
+  return {
+    backgroundColor: 'whitesmoke',
+    background: `linear-gradient(230deg, ${tinycolor(color).spin(24).darken(24).toRgbString()}, ${tinycolor(color).darken(6).toRgbString()}, ${tinycolor(color).spin(-24).darken(24).toRgbString()})`,
+    display: 'flex',
+    '> .ant-modal': {
+      top: 0,
+      margin: 'auto',
+      paddingBottom: 24,
+      paddingTop: 24,
+      '> .ant-modal-content': {
+        borderRadius: 0,
+        backgroundColor: 'transparent',
+        boxShadow: 'none',
+        '> .ant-modal-close': {
+          color: tinycolor(color).getBrightness() > 60 ? '#FFFFFF' : '#333333',
+        },
+        '> .ant-modal-body': {
+          textAlign: 'center',
+        },
+        '> .ant-modal-footer': {
+          '> .ant-btn': {
+            width: 'calc(50% - 4px)',
+          },
+        },
+      }
     }
   }
 }
@@ -24,6 +42,6 @@ const modalSettings = {
   maskTransitionName: 'fade',
 };
 
-export default styled(() => style, ({ className, ...props }) => (
+export default styled(({ theme }) => style(theme), ({ className, ...props }) => (
   <Modal {...modalSettings} {...props} wrapClassName={className} />
 ), p => p);
