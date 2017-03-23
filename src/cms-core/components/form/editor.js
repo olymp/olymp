0984f2851d79672cd469c2551ac367377d46c5f3
Benjamin Kniffler <bkniffler@me.com>
2017-03-23 11:37:20 +0100

    fix collection

--- src/cms-core/components/form/editor.js
+++ src/cms-core/components/form/editor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Input, Select, Checkbox, InputNumber } from 'antd';
-import { ColorEditor, DateEditor, DateRangeEditor, DetailEditor, TimeRangeEditor, TagsEditor, FormEditor, SuggestEditor } from 'olymp/edits';
+import { ColorEditor, DateEditor, DateRangeEditor, DetailEditor, TimeRangeEditor, TagsEditor, SuggestEditor } from 'olymp/edits';
+import FormEditor from './edit';
 import { SlateMate } from 'olymp/slate';
 import moment from 'moment';
 import capitalize from 'lodash/upperFirst';
