84b06a0b408eeabed7d122bdac17a70a428b37e1
Benjamin Kniffler <bkniffler@me.com>
2017-05-01 15:28:02 +0100

    linting

--- src/cms-next/cloudinary/routes/cloudinary.js
+++ src/cms-next/cloudinary/routes/cloudinary.js
@@ -1,30 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
 import { Cloudinary } from '../views';
+
+const onClick = ({ query, pathname, router }) => (selectionIds) => {
+  // MULTI
+  const selected = (query[`@media`] || '').split(',').filter(x => x);
+  selectionIds.forEach((selectionId) => {
+    const itemIndex = selected.findIndex(item => item === selectionId);
+    if (itemIndex < 0) {
+      selected.push(selectionId); // add/select
+    } else {
+      selected.splice(itemIndex, 1); // remove/deselect
+    }
+  });
+  router.push({ pathname, query: { ...query, '@media': selected.join(',') }});
+  // ONLY ONE
+  /* const selected = (query['@media'] || '').split(',').filter(x => x)[0];
+  router.push({ pathname, query: { ...query, '@media': selected !== selectionIds[0] ? selectionIds[0] : null } });*/
+};
 
 export default ({ query, pathname, router }) => (
   <Cloudinary
-    selected={(query[`@media`] || '').split(',').filter(x => x)}
-    onClick={selectionIds => {
-      // MULTI
-      const selected = (query[`@media`] || '').split(',').filter(x => x);
-
-      selectionIds.forEach(selectionId => {
-        const itemIndex = selected.findIndex(item => item === selectionId);
-
-        if (itemIndex < 0) {
-          selected.push(selectionId); // add/select
-        } else {
-          selected.splice(itemIndex, 1); // remove/deselect
-        }
-      })
-
-      router.push({pathname, query: { ...query, ['@media']: selected.join(',') }});
-
-      // ONLY ONE
-      // const selected = (query[`@media`] || '').split(',').filter(x => x)[0];
-      // router.push({pathname, query: { ...query, ['@media']: selected !== selectionIds[0] ? selectionIds[0] : null }});
-    }}
+    selected={(query['@media'] || '').split(',').filter(x => x)}
+    onClick={onClick({ query, pathname, router })}
     // onSelect={items => console.log(items)} // => Selection-Mode
-    onClose={() => router.push({pathname, query: { ...query, ['@media']: undefined }})}
+    onClose={() => router.push({ pathname, query: { ...query, '@media': undefined } })}
   />
 );
