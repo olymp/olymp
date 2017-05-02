1f70a2568882e35ee88735988ad616acd5f14a8d
Benjamin Kniffler <bkniffler@me.com>
2017-05-02 16:32:25 +0100

    center title

--- src/ui/list/title.js
+++ src/ui/list/title.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { styled } from 'olymp';
 
-export default styled(({ theme }) => ({
+export default styled(({ theme, center }) => ({
   padding: '5px 6px',
   borderBottom: '1px solid #e9e9e9',
   fontSize: '1.17em',
   fontWeight: 200,
   color: theme.color,
+  textAlign: center ? 'center' : undefined,
   '> div': {
     float: 'right',
   },
