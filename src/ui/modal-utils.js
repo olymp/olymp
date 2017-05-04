eaa4c2f4bc58e92c7b88bcff4462944aac90d6cd
Benjamin Kniffler <bkniffler@me.com>
2017-05-05 00:14:18 +0100

    use antd message

--- src/ui/modal-utils.js
+++ src/ui/modal-utils.js
@@ -1,4 +1,4 @@
-import { notification } from 'antd';
+import { message } from 'antd';
 
 export const onError = (err) => {
   let description;
@@ -7,23 +7,17 @@ export const onError = (err) => {
   } else if (err && typeof err === 'object') {
     description = Object.keys(err).map(key => err[key].errors.map(e => e.message).join('\n')).join('\n')
   }
-  notification.error({
-    message: 'Fehler',
-    description,
-  });
-}
-export const onSuccess = (message, description) => {
-  notification.success({
-    message,
-    description,
-  });
-}
+  message.error(description);
+};
+export const onSuccess = (text, description) => {
+  message.success(description);
+};
 export const layout = { labelCol: { span: 7 }, wrapperCol: { span: 17 } };
 export const onEnterFocus = (ref) => (e) => {
   if (e.key === 'Enter') {
     return ref() && ref().refs && ref().refs.input.focus();
   } return false;
-}
+};
 export const onEnterOk = (onOk) => (e) => {
   if (e.key === 'Enter') onOk();
-}
+};
