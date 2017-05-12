2293100e86028122718e17a2dee6a92ecafae1d1
Benjamin Kniffler <bkniffler@me.com>
2017-05-12 02:11:32 +0200

    old auth

---
+++ src/auth-old/views/forgot.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link, Modal, withAuth } from 'olymp';
+import { Form, Input, Button, notification } from 'antd';
+import { EnvelopeO, Key } from 'olymp-icons';
+import Base, { onEnterFocus, onEnterOk, layout, onError, onSuccess } from './base';
+
+@withAuth
+@Form.create()
+export default class AuthForgot extends Component {
+  ok = () => {
+    const { auth, onClose, onOk, form } = this.props;
+    form.validateFields((err, values) => {
+      if (err) return onError(err);
+      auth.forgot(values.email).then(({ name }) => {
+        onSuccess('E-Mail abgeschickt', `Bitte bestätigen Sie die Zurücksetzung`);
+        onOk({ email: values.email });
+      }).catch(onError);
+    });
+  }
+
+  render() {
+    const { isOpen, email, form, saving, pathname, onClose } = this.props;
+    const { getFieldDecorator } = form;
+
+    return (
+      <Base isOpen={isOpen} title="Zurücksetzen" onOk={this.ok} onCancel={onClose}>
+        <Form.Item hasFeedback key="email" label="E-Mail" {...layout}>
+          {getFieldDecorator('email', {
+            initialValue: email,
+            rules: [{ required: true, message: 'Bitte geben Sie Ihre E-Mail an!' }],
+          })(<Input type="email" placeholder="E-Mail" onKeyPress={onEnterOk(this.ok)} size="large" addonAfter={<EnvelopeO size={10} />} />)}
+        </Form.Item>
+        <Modal.Links>
+          <Link to={{ pathname, query: { login: null, forgot: undefined } }}>Zur Anmeldung</Link>
+        </Modal.Links>
+      </Base>
+    );
+  }
+}
+
