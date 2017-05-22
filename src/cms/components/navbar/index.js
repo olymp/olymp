c2322d4cf64ec87f20d9080c1fc668e477893365
Philipp Achenbach <github@philipp-achenbach.de>
2017-05-22 11:56:35 +0200

    fixes & bootstrap-navbar (without ordering)

---
+++ src/cms/components/navbar/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { cn, NavLink, withRouter } from 'olymp';
+import { Icon } from 'antd';
+import './navbar.less';
+
+@withRouter
+export default class Navbar extends Component {
+  getMenu = (page, isSubmenu) => {
+    const { router } = this.props;
+    const { query, pathname } = location;
+    const navigation = !!query && query.navigation
+
+    let matches = [];
+    page.children.forEach((e) => {
+      matches = matches.concat(e.children.filter(child => child.Id === navigation));
+    });
+    const visible = navigation === page.id || !!matches.length;
+
+    const submenu = page.children && page.children.length ? (
+      <ul className="dropdown-menu">
+        {page.children.map(child => this.getMenu(child, true))}
+      </ul>
+    ) : null;
+
+    return (
+      <li className={cn(isSubmenu ? 'dropdown-item' : 'nav-item', page.path === location.pathname ? 'active' : null, visible ? 'focus' : null)} key={page.id}>
+        {page.blocks ? (
+          <NavLink to={page.path} className="nav-link">
+            {page.name}
+          </NavLink>
+        ) : (
+          <span
+            onClick={() => router.push({ pathname, query: { navigation: page.id } })}
+            className="nav-link"
+          >
+            {page.name}
+          </span>
+        )}
+        {submenu}
+      </li>
+    );
+  }
+
+  // RAAH: brand/navbar-collapse col-md-..., Kontakt => Kontakt/Impressum, a.active => li.active, navbar-toggler-left
+  render() {
+    const { readOnly, router, brand, fill, pages } = this.props;
+    const { query, pathname } = location;
+    const navigation = !!query && query.navigation !== undefined; // null/page.id => true, undefined => false
+
+    return (
+      <nav className="navbar navbar-inverse navbar-toggleable-sm">
+        {brand ? <NavLink to="/" className="navbar-brand">{brand}</NavLink> : null}
+
+        <button
+          onClick={() => router.push({ pathname, query: { navigation: navigation ? undefined : null } })}
+          className="navbar-toggler hidden-md-up"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className={`navbar-collapse${!navigation ? ' hidden-sm-down' : ''}`}>
+          <ul className={cn('navbar-nav', fill ? 'nav-fill' : null)} style={{ width: fill ? '100%': 'auto'}}>
+            {pages.map(page => this.getMenu(page))}
+            {!readOnly ? (
+              <li className="nav-item">
+                <NavLink to={{ ...location, query: { '@new-page': null } }} className="nav-link">
+                  <Icon type="plus-circle" />
+                </NavLink>
+              </li>
+            ) : null}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
--- src/ekhn/components/items/item.js
+++ src/ekhn/components/items/item.js
@@ -9,25 +9,20 @@ export default useItemEdit()((props) => {
   const { children, id, header, subheader, shortText, more, bild, tags, identifier, className, leading } = props;
 
   return (
-    <div className={cn(className, `item ${leading ? 'colored' : ''}`)} key={id}>
-      {children}
+    <Responsive {...props} className={cn(className, `item row ${leading ? 'colored' : ''}`)}>
+      <div className="info col-xl-4 col-md-5 py-3 hidden-sm-down">
+        {bild ? <Image className="header" width="100%" value={bild} cloudinary={{ width: 300 }} lightbox /> : null}
+        {shortText ? <Tags tags={tags} /> : null}
+        {children}
+      </div>
 
-      <Responsive {...props} className='row p-3'>
-        <div className="info col-xl-4 col-md-5 hidden-sm-down">
-          {bild ? <Image className="header" width="100%" value={bild} cloudinary={{ width: 300 }} lightbox /> : null}
-          {shortText ? <Tags tags={tags} /> : null}
-        </div>
+      <div className="text col-xl-8 col-md-7">
+        {header ? <ItemHeader className="mt-0" id={id} identifier={identifier}>{header}</ItemHeader> : null}
+        {subheader ? <div className="subheader">{subheader}</div> : null}
+        {shortText ? <SlateMateFrontend key={id} value={shortText} readOnly className="slate" /> : null}
 
-        <div className="text p-0 col-xl-8 col-md-7">
-          {header ? <ItemHeader className="mt-0" id={id} identifier={identifier}>{header}</ItemHeader> : null}
-          {subheader ? <div className="subheader">{subheader}</div> : null}
-          {shortText ? <SlateMateFrontend key={id} value={shortText} readOnly className="slate" /> : null}
-
-          <ItemMore more={more} id={id} identifier={identifier} />
-        </div>
-      </Responsive>
-
-      <div style={{ clear: 'both' }} />
-    </div>
+        <ItemMore more={more} id={id} identifier={identifier} />
+      </div>
+    </Responsive>
   );
 });
