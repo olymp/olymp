b5bd623b1e8d6368383264b114ccbd9b76901f52
Benjamin Kniffler <bkniffler@me.com>
2017-05-24 14:45:38 +0200

    small stuff

--- src/ui/list/title.js
+++ src/ui/list/title.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { styled } from 'olymp';
 
-export default styled(({ theme, center }) => ({
+export default styled(({ theme, noBorder, color, center }) => ({
   padding: '5px 6px',
-  borderBottom: '1px solid #e9e9e9',
+  borderBottom: noBorder || '1px solid #e9e9e9',
   fontSize: '1.17em',
   fontWeight: 200,
-  color: theme.color,
+  color: color || theme.color,
   textAlign: center ? 'center' : undefined,
   '> div': {
     float: 'right',
