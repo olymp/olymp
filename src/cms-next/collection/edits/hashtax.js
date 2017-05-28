021990d22c26d19568ca2b58d883ba9543c6ee1c
Benjamin Kniffler <bkniffler@me.com>
2017-05-28 19:45:33 +0200

    fix

--- src/cms-next/collection/edits/hashtax.js
+++ src/cms-next/collection/edits/hashtax.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Input as AntInput, Form } from 'antd';
+import { Form } from 'antd';
 import getRules from '../rules';
 import { layout } from 'olymp/ui';
-import Slate from './hashtax-edit';
+import Slate from 'hashtax/edit';
 
 const HashtaxInput = ({ item, field, label, layout, initialValue, rules, placeholder, form, ...rest }) => {
   const inner = form.getFieldDecorator(field, {
     initialValue: item ? item[field] : undefined,
     rules: getRules(rules, label),
-  })(<Slate placeholder={placeholder || label} {...rest} />);
+  })(<Slate placeholder={placeholder || label} {...rest} />);
   if (label === null) return inner;
   return (
     <Form.Item key={field} label={label} {...label}>
