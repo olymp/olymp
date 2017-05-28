d9e145bab23089e5550aa8a7ca8815d186094a95
Philipp Achenbach <github@philipp-achenbach.de>
2017-05-28 11:05:44 +0200

    fela-notices

--- runtime/fela.js
+++ runtime/fela.js
@@ -8,13 +8,19 @@ import removeUndefined from 'fela-plugin-remove-undefined';
 import friendlyPseudoClass from 'fela-plugin-friendly-pseudo-class';
 import namedMediaQuery from 'fela-plugin-named-media-query';
 
-// todo: Helfer für gradient, box-Shadows (mit Level) und inner-Shadows
-// color, colorAccent, colorSuccess, colorInfo, colorWarning, colorDanger, colorMuted
-// colorText, colorTextSecondary, colorTextMuted https://material.io/guidelines/style/color.html#color-usability
-// colorTextLight, colorTextLightSecondary, colorTextLightMuted ??? (für dunklen Hintergrund)
-// für jeden Farbe: color, colorLight, colorDark, colorInv
-// fontSize, fontSizeSmall, fontSizeLarge
-// space0, space1, ..., space5 https://v4-alpha.getbootstrap.com/utilities/spacing/
+// todo
+// color: {primary, accent, success, info, warning, danger, muted}
+  // color.primary.getPrimaryText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
+  // color.primary.getSecondaryText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
+  // color.primary.getMutedText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
+  // color.primary.getLight(percent = 4%) = Gibt hellere Farbe zurück
+  // color.primary.getDark(percent = 4%) = Gibt dunklere Farbe zurück
+  // color.primary.getNuance(grad = 180°) = Gibt spin der Farbe zurück
+  // color.primary.getGradient(grad = 45°, color = undefined) = Gibt Gradient der Farbe zurück
+// space: {d0, d1, d2 ... d5} (d = dynamisch = x rem) https://v4-alpha.getbootstrap.com/utilities/spacing/
+// fontSize: {primary, secondary, small}
+// borderRadius: {primary???}
+// boxShadows: {l0, l1, l2, ..., i0, i1, ...} (l = level, i = inner)
 
 export default () => {
   const renderer = createRenderer({
