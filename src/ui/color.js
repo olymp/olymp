2506486de4cbcae86359b302024646335efecf1a
Philipp Achenbach <philippachenbach@Philipps-MacBook-Pro.local>
2017-05-28 22:17:48 +0200

    theme navbar

--- src/ui/color.js
+++ src/ui/color.js
@@ -1,15 +1,15 @@
 import tinycolor from 'tinycolor2';
 
-export const border = (theme, color) => `${theme.borderWith}px ${theme.borderStyle} ${color || theme.color}`;
-export const lighten = (color, percent) => tinycolor(color).lighten(percent || 15).toRgbString();
-export const darken = (color, percent) => tinycolor(color).darken(percent || 7).toRgbString();
+export const border = (theme, color) => `${theme.borderWidth}px ${theme.borderStyle} ${color || theme.color}`;
+export const lighten = (color, percent) => tinycolor(color).lighten(percent || 8).toRgbString();
+export const darken = (color, percent) => tinycolor(color).darken(percent || 4).toRgbString();
 export const spin = (color, deg) => tinycolor(color).spin(deg || 180).toRgbString();
 export const fade = (color, percent) => tinycolor(color).setAlpha(percent / 100 || 0.67).toRgbString();
 export const shadow = color => `0px 0px 5px 0px ${tinycolor(color).toRgbString()}`;
 export const innerShadow = color => `inset ${shadow(color)}`;
 export const gradient = (color1, color2, deg) => {
-  let tColor1 = tinycolor(lighten(color1)).spin(12);
-  let tColor2 = tinycolor(darken(color1)).spin(-6);
+  let tColor1 = tinycolor(lighten(color1)).spin(8);
+  let tColor2 = tinycolor(darken(color1)).spin(-4);
   let tDeg = deg || 90;
 
   if (color2 && deg) { // color1, color2, deg
