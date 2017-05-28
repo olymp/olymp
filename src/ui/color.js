21e167d29bca431750e444f27924fa4df314ab09
Philipp Achenbach <github@philipp-achenbach.de>
2017-05-28 16:57:20 +0200

    theme

---
+++ src/ui/color.js
@@ -0,0 +1,30 @@
+import tinycolor from 'tinycolor2';
+
+export const gradient = (color1, color2, deg) => {
+  let tColor1 = tinycolor(color1).lighten();
+  let tColor2 = tinycolor(color1).darken();
+  let tDeg = deg;
+
+  if (color2 && deg) { // color1, color2, deg
+    tColor1 = tinycolor(color1);
+    tColor2 = tinycolor(color2);
+  } else if (!deg && color2 === parseInt(color2, 10)) { // color1, deg
+    tDeg = color2;
+  }
+
+  return `linear-gradient(${tDeg + 90}, ${tColor1.toRgbString()} 0%, ${tColor2.toRgbString()} 100%)`;
+
+  /* return `
+    background: ${tColor1.toString()}; // Old browsers
+    background: -moz-linear-gradient(${tDeg - 90}, ${tColor1.toRgbString()} 0%, ${tColor2.toRgbString()} 100%); // FF3.6-15
+    background: -webkit-linear-gradient(${tDeg - 90}, ${tColor1.toRgbString()} 0%, ${tColor2.toRgbString()} 100%); // Chrome10-25,Safari5.1-6
+    background: linear-gradient(${tDeg + 90}, ${tColor1.toRgbString()} 0%, ${tColor2.toRgbString()} 100%); // W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${tColor1.toString()}', endColorstr='${tColor2.toString()}',GradientType=1 ); // IE6-9 fallback on horizontal gradient
+  `; */
+};
+export const border = (theme, color) => `${theme.borderWith}px ${theme.borderStyle} ${color || theme.color}`;
+export const lighten = (color, percent) => tinycolor(color).lighten(percent || 15).toRgbString();
+export const darken = (color, percent) => tinycolor(color).darken(percent || 7).toRgbString();
+export const spin = (color, deg) => tinycolor(color).spin(deg || 180).toRgbString();
+export const shadow = color => `0px 0px 5px 0px ${tinycolor(color).toRgbString()}`;
+export const innerShadow = color => `inset ${shadow(color)}`;
--- src/ui/theme-provider.js
+++ src/ui/theme-provider.js
@@ -1,119 +1,36 @@
 import { ThemeProvider } from 'react-fela';
-import tinycolor from 'tinycolor2';
 import React from 'react';
 
-// todo
-// color: {primary, accent, success, info, warning, danger, muted}
-  // color.primary.getPrimaryText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
-  // color.primary.getSecondaryText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
-  // color.primary.getMutedText() = Gibt je nach Hintergrund (hier primary) helle oder dunkle Schriftfarbe zurück https://material.io/guidelines/style/color.html#color-usability
-  // color.primary.getLight(percent = 4%) = Gibt hellere Farbe zurück
-  // color.primary.getDark(percent = 4%) = Gibt dunklere Farbe zurück
-  // color.primary.getNuance(grad = 180°) = Gibt spin der Farbe zurück
-  // color.primary.getGradient(grad = 45°, color = undefined) = Gibt Gradient der Farbe zurück
-// space: {d0, d1, d2 ... d5} (d = dynamisch = x rem) https://v4-alpha.getbootstrap.com/utilities/spacing/
-// fontSize: {primary, small}
-// boxShadows: normal und inner
-// textColorLight/Dark
-// borderWidth/Style/Radius => getBorder(color) => `${theme.borderWidth} ${theme.borderStyle} ${color}`
-
-/*
-PRIMARY, SECONDARY, SUCCESS, INFO, WARNING, DANGER, MUTED
-
-theme: {
-  space: INT | [INT]
-  fontSize: INT | [INT (default), INT (h1), INT (h2), INT (h3), INT (h4), INT (h5), INT (h6), INT (small)]
-  borderWidth: INT (pixel)
-  borderStyle: BORDER_STYLE
-  borderRadius: INT (pixel)
-  color: STRING
-  colorSuccess: STRING
-  textColorLight: STRING
-  textColorDark: STRING
-}
-
-getShadow
-getInnerShadow
-getBorder(theme.color)
-getGradient(theme.color)
-getColor(theme.color)
-getColor('muted', theme.color)
-getLight(theme.color, 2)
-getDark(theme.color, 2)
-getSpin(theme.color, 2)
-
-*/
-export const COLOR_TYPE = {
-  PRIMARY: 'PRIMARY',
-  SECONDARY: 'SECONDARY',
-  SUCCESS: 'SUCCESS',
-  INFO: 'INFO',
-  WARNING: 'WARNING',
-  DANGER: 'DANGER',
-  MUTED: 'MUTED',
-};
-
-export const TEXT_TYPE = {
-  PRIMARY: 'PRIMARY',
-  SECONDARY: 'SECONDARY',
-  MUTED: 'MUTED',
-};
-
-const getTheme = ({ color, textColor, ...rest }) => {
-  const theme = {
-    color: {
-      primary: '#8e44ad',
-      secondary: '#e67e22',
-      success: '#2ecc71',
-      info: '#3498db',
-      warning: '#f39c12',
-      danger: '#e74c3c',
-      muted: '#bdc3c7',
-      ...color,
-    },
-    textColor: {
-      light: '#FFFFFF',
-      dark: '#000000',
-      ...textColor,
-    },
-    ...rest
-  };
-
-  return {
-    getColor: color => theme.color[color] || theme.color.primary,
-    getTextColor: (type, bgColor) => {
-      let newType = TEXT_TYPE[type];
-      let newColor = bgColor;
-
-      if (!bgColor && !TEXT_TYPE[type]) { // if no second argument and first not a type
-        newColor = type;
-        newType = 'PRIMARY';
-      }
-
-      const textColors = Object.keys(theme.textColor).map(color => theme.textColor[color]);
-      const color = tinycolor.mostReadable(newColor, textColors);
-
-      switch (newType) {
-        case 'SECONDARY':
-          return color.isDark() ?
-            color.setAlpha(0.46).toRgbString()
-              : color.setAlpha(0.3).toRgbString();
-
-        case 'MUTED':
-          return color.isDark() ?
-            color.setAlpha(0.62).toRgbString()
-              : color.setAlpha(0.5).toRgbString();
-
-        default:
-          return color.isDark() ?
-            color.setAlpha(0.13).toRgbString()
-              : color.toRgbString();
-      }
-    },
-    getSpace: space => `${0.25 * ((2 ** space) - 1)}.rem`,
-    ...theme,
-  };
-};
+const getTheme = theme => ({
+  color: '#8e44ad',
+  colorSecondary: '#e67e22',
+  colorSuccess: '#2ecc71',
+  colorInfo: '#3498db',
+  colorWarning: '#f39c12',
+  colorDanger: '#e74c3c',
+  colorMuted: '#bdc3c7',
+  textColor: '#333333',
+  textColorLight: '#FFFFFF',
+  textColorDark: '#000000',
+  space0: 0,
+  space1: '0.25rem',
+  space2: '0.5rem',
+  space3: '1rem',
+  space4: '2rem',
+  space5: '4rem',
+  borderWidth: 1,
+  borderStyle: 'solid',
+  borderRadius: 3,
+  fontSize: '1rem',
+  fontSizeSmall: '1rem',
+  fontSizeH1: '1rem',
+  fontSizeH2: '1rem',
+  fontSizeH3: '1rem',
+  fontSizeH4: '1rem',
+  fontSizeH5: '1rem',
+  fontSizeH6: '1rem',
+  ...theme,
+});
 
 export default ({ theme, ...rest }) => (
   <ThemeProvider theme={getTheme(theme)} {...rest} />
