7d9f84a5b7b77ddbc9c81b00db3ae53988e9cfa1
Philipp Achenbach <philippachenbach@Philipps-MacBook-Pro.local>
2017-05-29 16:26:21 +0200

    grid

---
+++ src/fela/grid/index.js
@@ -0,0 +1,6 @@
+import Grid from './grid';
+import Item from './item';
+
+Grid.Item = Item;
+
+export default Grid;
--- src/fela/index.js
+++ src/fela/index.js
@@ -1,4 +1,5 @@
 export { default as createFela } from './create-fela';
 export { default as ThemeProvider } from './theme-provider';
-export * from './utils';
+export { default as Grid } from './grid';
 export * from './navbar';
+export * from './utils';
