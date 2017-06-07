b17ebc54e692a164cf8632996ecf5ab9a5931148
Philipp Achenbach <philippachenbach@Philipps-MacBook-Pro.local>
2017-06-07 15:02:21 +0200

    panel

---
+++ packages/cloudinary/src/components/thumb.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { object, func, number, bool } from 'prop-types';
+import { styled } from 'olymp';
+import { Icon } from 'antd';
+import Image from './image';
+
+const ImageContainer = styled(({ theme, isActive }) => ({
+  position: 'relative',
+  margin: '.5rem',
+  cursor: 'pointer',
+  // outline: isActive ? `3px solid ${theme.color}` : 'none',
+  // transform: isActive ? 'scale(1.15)' : 'none',
+  transition: 'all .1s ease-in-out',
+  backgroundColor: isActive ? theme.color : '#FFF',
+  border: isActive ? `1px solid ${theme.color}` : '1px solid #eee',
+  // boxShadow: `0px 0px 10px 0px rgba(0, 0, 0, ${isActive ? 0.4 : 0.2})`,
+  '> img': {
+    opacity: isActive ? .6 : 1,
+    backgroundColor: '#FFF',
+    display: 'block',
+  },
+  ':hover': {
+    // boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.4)',
+    transform: 'scale(1.025)',
+    transition: 'all .1s ease-in-out',
+  },
+}), 'div', ({ height, isActive, ...p }) => p);
+
+const ImageLabel = styled(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  right: '50%',
+  transform: 'translate(50%, -50%)',
+  transition: 'all .15s ease-in-out',
+  backgroundColor: theme.color,
+  color: '#FFF',
+  borderRadius: '50%',
+  textAlign: 'center',
+  fontSize: 25,
+  padding: 5,
+  lineHeight: 1,
+  // boxShadow: "0px 0px 12px 0px rgba(0,0,0,0.75)",
+}), 'div', p => p);
+
+const CheckLabel = styled(({ theme }) => ({
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  transform: 'translate(40%, -40%) scale(0.667)',
+  transition: 'all .15s ease-in-out',
+  backgroundColor: theme.color,
+  color: '#FFF',
+  borderRadius: '50%',
+  textAlign: 'center',
+  fontSize: 25,
+  padding: 5,
+  lineHeight: 1,
+}), 'div', p => p);
+
+const CloseLabel = styled(({ theme }) => ({
+  ':hover': {
+    transform: 'translate(40%, -40%) scale(0.75)',
+    transition: 'all .15s ease-in-out',
+  }
+}), CheckLabel, p => p);
+
+const Thumb = ({ item, onClick, onRemove, isActive, height }) => item ? (
+  <ImageContainer isActive={isActive}>
+    <Image value={item} mode="fill" height={height} onClick={onClick} style={{ maxWidth: '100%' }} retina />
+    {item.format === 'pdf' ? (
+      <ImageLabel>
+        <Icon type="file-pdf" />
+      </ImageLabel>
+    ) : undefined}
+    {isActive ? (
+      onRemove ? (
+        <CloseLabel onClick={onRemove}>
+          <Icon type="close" />
+        </CloseLabel>
+      ) : (
+        <CheckLabel>
+          <Icon type="check" />
+        </CheckLabel>
+      )
+    ) : undefined}
+  </ImageContainer>
+) : null;
+Thumb.propTypes = {
+  item: object,
+  onClick: func,
+  onRemove: func,
+  height: number,
+  isActive: bool,
+};
+export default Thumb;
---
+++ src/gzk/blocks/panel/panel.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { styled } from 'olymp';
+import { fade } from 'olymp-fela';
+
+export default styled(({ theme, size = 1, columns = 3, background = theme.dark5, color = theme.dark2, padding = `${theme.space2} 1.33rem 1.33rem ${theme.space2}` }) => ({
+  width: `${100 / columns * size}%`,
+  float: 'left',
+  padding: `${theme.space3} 0 0 ${theme.space3}`,
+  position: 'relative',
+  textAlign: 'justify',
+  display: 'flex',
+  flexDirection: 'column',
+  '> h2': {
+    marginBottom: theme.space1,
+  },
+  '> p': {
+    backgroundColor: background,
+    color,
+    padding,
+    borderBottomRightRadius: 100,
+    display: 'flex',
+    flex: '1 1',
+    '> iframe': {
+      display: 'block',
+      borderBottomRightRadius: 100,
+    }
+  },
+  onAfter: {
+    content: '""',
+    position: 'absolute',
+    width: 33,
+    height: 33,
+    backgroundColor: theme.dark4,
+    bottom: 0,
+    borderTopLeftRadius: 30,
+    right: 0,
+  },
+  onHover: {
+    onAfter: {
+      backgroundColor: theme.color,
+    },
+  },
+  ifSmallDown: {
+    width: '100%',
+    float: 'none',
+    paddingX: theme.space2,
+    paddingTop: theme.space2,
+    paddingBottom: theme.space0,
+  }
+}), ({ className, title, children }) => (
+  <div className={className}>
+    <h2>{title}</h2>
+    <p>{children}</p>
+  </div>
+), p => p);
