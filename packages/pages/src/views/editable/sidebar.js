e596812c73297685c9384a8af2c602fdd13b50ca
Philipp Achenbach <philippachenbach@Philipps-MacBook-Pro.local>
2017-06-09 16:58:12 +0200

    page improvements

--- packages/pages/src/views/editable/sidebar.js
+++ packages/pages/src/views/editable/sidebar.js
@@ -19,7 +19,7 @@ class PageForm extends Component {
     }
   }
   render() {
-    const { form, item, items, navigation } = this.props;
+    const { form, item, items, navigation, tab, onTabClick } = this.props;
     const tree = unflatten(items.map(({ id, name, parentId }) => ({
       value: id,
       label: name,
@@ -28,39 +28,42 @@ class PageForm extends Component {
     })), { id: 'value', parentId: 'parent' });
 
     return (
-      <div>
-        <Tabs defaultActiveKey="0" size="small">
-          <Tabs.TabPane tab="Übersicht" key="0">
-            <Panel>
-              <Tree items={navigation} />
-            </Panel>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Seite" key="1">
-            <Panel paddingX={16} alignLabel="left">
-              <Input form={form} item={item} field="name" label="Name" onChange={this.handleNameChange} rules={['required']} type="text" size="large" />
-              <Input form={form} item={item} field="slug" label="Slug" type="text" size="large" />
-              <State form={form} item={item} field="state" label="Status" rules={['required']} />
-              <PageType form={form} item={item} field="type" label="Art" size="large" onChange={this.handleTypeChange} />
-              {(form.getFieldValue('type') || item.type) !== 'MENU' && <Parent form={form} treeData={tree} item={item} field="parentId" label="Menü" placeholder="Übergeordnetes Menü" size="large" />}
-              {(form.getFieldValue('type') || item.type) === 'LINK' && <Input form={form} item={item} field="href" label="Ext. Link" type="text" size="large" />}
-              {(form.getFieldValue('type') || item.type) === 'ALIAS' && <Parent form={form} treeData={tree} item={item} field="aliasId" label="Alias" placeholder="Alias zu.." size="large" />}
-              <SectionH title="Erweitert" description="Datenanbindung und Sortierung der Elemente" />
-              <Input form={form} item={item} field="binding" placeholder="typ id name" label="Bindung" type="text" size="large" />
-              <Input form={form} item={item} field="sorting" placeholder="+name, -id" label="Sortieren" type="text" size="large" />
-            </Panel>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Collection" key="2">
-            <Panel paddingX={16}>
-              Hier kommt bei Bindings quasi der Parent rein
-            </Panel>
-          </Tabs.TabPane>
-          {/* <Tabs.TabPane tab="Text" key="2">
-            <Panel paddingX={16}>
-              <SlateTree form={form} item={item} field="blocks" label={null} />
-            </Panel>
-          </Tabs.TabPane> */}
-        </Tabs>
-      </div>
+      <Tabs
+        activeKey={tab}
+        onTabClick={onTabClick}
+        size="small"
+        tabBarStyle={{ marginBottom: 0 }}
+      >
+        <Tabs.TabPane tab="Navigation" key="0">
+          <Panel>
+            <Tree items={navigation} />
+          </Panel>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Seite" key="1">
+          <Panel padding={16} alignLabel="left">
+            <Input form={form} item={item} field="name" label="Name" onChange={this.handleNameChange} rules={['required']} type="text" size="large" />
+            <Input form={form} item={item} field="slug" label="Slug" type="text" size="large" />
+            <State form={form} item={item} field="state" label="Status" rules={['required']} />
+            <PageType form={form} item={item} field="type" label="Art" size="large" onChange={this.handleTypeChange} />
+            {(form.getFieldValue('type') || item.type) !== 'MENU' && <Parent form={form} treeData={tree} item={item} field="parentId" label="Menü" placeholder="Übergeordnetes Menü" size="large" />}
+            {(form.getFieldValue('type') || item.type) === 'LINK' && <Input form={form} item={item} field="href" label="Ext. Link" type="text" size="large" />}
+            {(form.getFieldValue('type') || item.type) === 'ALIAS' && <Parent form={form} treeData={tree} item={item} field="aliasId" label="Alias" placeholder="Alias zu.." size="large" />}
+            <SectionH title="Erweitert" description="Datenanbindung und Sortierung der Elemente" />
+            <Input form={form} item={item} field="binding" placeholder="typ id name" label="Bindung" type="text" size="large" />
+            <Input form={form} item={item} field="sorting" placeholder="+name, -id" label="Sortieren" type="text" size="large" />
+          </Panel>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Collection" key="2">
+          <Panel paddingX={16}>
+            Hier kommt bei Bindings quasi der Parent rein
+          </Panel>
+        </Tabs.TabPane>
+        {/* <Tabs.TabPane tab="Text" key="2">
+          <Panel paddingX={16}>
+            <SlateTree form={form} item={item} field="blocks" label={null} />
+          </Panel>
+        </Tabs.TabPane> */}
+      </Tabs>
     );
   }
 }
@@ -69,11 +72,15 @@ PageForm.propTypes = {
   form: PropTypes.object,
   items: PropTypes.arrayOf(PropTypes.object),
   navigation: PropTypes.arrayOf(PropTypes.object),
+  tab: PropTypes.string,
+  onTabClick: PropTypes.func,
 };
 PageForm.defaultProps = {
   item: {},
   items: [],
   navigation: [],
+  tab: '0',
+  onTabClick: key => console.log(key),
 };
 PageForm.WithData = queryPages(queryPage(mutatePage(Form.create(PageForm))));
 export default PageForm;
