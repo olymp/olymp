ca184c5c66453261867a845edfde82f62dd434cd
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-09 11:04:21 +0200

    grid.item => add responsive padding responisve optimations neuigkeiten-block

--- src/gzk/blocks/magazin/index.js
+++ src/gzk/blocks/magazin/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createComponent, Container, Grid, border } from 'olymp-fela';
+import { createComponent, Container, Grid, border, fade } from 'olymp-fela';
 import { H2 } from '../../components';
 import Panel from '../panel/panel';
 
@@ -10,92 +10,25 @@ export default {
   component: ({ getData, ...props }) => (
     <Container>
       <Grid>
-        <Grid.Item medium={8} padding="0 1rem">
-          <Panel title="Vertreterversammlung der KV Hessen wählt neue Vorsitzende">
-            <Image url="https://res.cloudinary.com/djyenzorc/image/upload/w_100,h_119,c_fill/f_auto,q_auto,fl_lossy/v1481083897/pyyfbp0dwkmxb1uzwzhs.jpg" />
-            <p>Herzlichen Glückwunsch Dr. Klaus-Wolfgang Richter zu seiner Wiederwahl zum Vorsitzenden der Vertretersammlung mit 49 von 50 Stimmen!</p>
-            <a href="/">Mehr erfahren...</a>
-            <H5>Presseartikel vom 15. Dezember 2016</H5>
-          </Panel>
-          <Panel title="Knorpelschäden und Arthrose">
-            <Image url="https://res.cloudinary.com/djyenzorc/image/upload/x_134,y_197,w_853,h_1249,c_crop/f_auto,q_auto,fl_lossy/v1481083829/sewtk0tjyyd8hiqmkoj4.jpg" />
-            <p>Prävention und moderne Behandlungsmöglichkeiten von Knorpelschäden und Arthrose - Ein Vortrag von Dr. Heino Kniffler.</p>
-            <a href="/">Mehr erfahren...</a>
-            <H5>Vortrag am 24. Januar 2017, 18:00 Uhr</H5>
+        <Grid.Item medium={8}>
+          <Panel title="Der hallux rigidus - Ein typisches Männerproblem">
+            <Image url="https://res.cloudinary.com/djyenzorc/image/upload/x_312,y_438,w_5890,h_3337,c_crop/f_auto,q_auto,fl_lossy/v1478466042/bkgzpJatL_wqz5on.jpg" />
+            <p>Beim Hallux rigidus handelt es sich um eine fortschreitende Arthrose im Großzehengrundgelenk. Diese geht mit einer Bewegungseinschränkung einher, um nach vielen Jahren in einer Steifheit zu enden (rigidus=steif). Männer sind sehr viel häufiger betroffen als Frauen. Bei Frauen bildet sich dafür öfter ein Hallux valgus, eine Abweichung der Großzehe zur Außenseite, was dann aber auch zur Ausbildung einer sog. sekundären Arthrose führen kann.</p>
+            <a href="/">Weiterlesen...</a>
           </Panel>
         </Grid.Item>
-        <Grid.Item medium={4} padding="1rem">
-          <H2>Vorträge & Veranstaltungen</H2>
+        <Grid.Item medium={4} paddingMini="1rem 1rem 0 1rem" paddingMedium="1rem 1.5rem">
+          <H2>Ausgaben als PDFs</H2>
           <ul>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>MAXIMAL 5 ITEMS!</p>
-            </Li>
+            <Li>Gesund im Zentrum - <b>Sport</b></Li>
+            <Li>Gesund im Zentrum - <b>Rinder</b></Li>
+            <Li>Gesund im Zentrum - <b>Beni</b></Li>
           </ul>
 
-          <H2>Publikationen</H2>
-          <ul>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>MAXIMAL 5 ITEMS!</p>
-            </Li>
-          </ul>
-
-          <H2>Presse</H2>
-          <ul>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>Ursachen und aktuelle Behandlungsmöglichkeiten von Schulterschmerzen</p>
-            </Li>
-            <Li>
-              <b>21. März 2017, 18:00 Uhr</b>
-              <p>Gelenkersatz am Knie</p>
-            </Li>
-            <Li>
-              <b>25. April 2017, 18:00 Uhr</b>
-              <p>MAXIMAL 5 ITEMS!</p>
-            </Li>
-          </ul>
+          <H2>Schlagworte</H2>
+          <Tag>Beni</Tag>
+          <Tag>Rinder</Tag>
+          <Tag>Sport</Tag>
         </Grid.Item>
       </Grid>
     </Container>
@@ -131,3 +64,16 @@ const Li = createComponent(({ theme }) => ({
     marginBottom: theme.space3,
   },
 }), 'li', p => Object.keys(p));
+
+const Tag = createComponent(({ theme }) => ({
+  backgroundColor: theme.dark4,
+  paddingY: theme.space2,
+  paddingX: theme.space3,
+  margin: theme.space2,
+  marginLeft: 0,
+  display: 'inline-block',
+  onHover: {
+    backgroundColor: theme.color,
+    color: theme.light,
+  }
+}), 'div', p => Object.keys(p));
