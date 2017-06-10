9497f88e42fd1cfd30b0d343ca041839fefa50a8
Philipp Achenbach <philippachenbach@Philipps-MacBook-Pro.local>
2017-06-10 12:28:38 +0200

    pages: sorting via tags

--- packages/pages/src/edits/index.js
+++ packages/pages/src/edits/index.js
@@ -1,4 +1,5 @@
 export { default as Parent } from './parent';
 export { default as Input } from './input';
+export { default as TagSelect } from './tags';
 export { default as State } from './state';
 export { default as PageType } from './page-type';
---
+++ packages/pages/src/edits/tags.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Select, Form } from 'antd';
+import { layout, getRules } from 'olymp-ui';
+
+const TagSelect = ({ item, field, label, layout, initialValue, rules, placeholder, form, options, ...rest }) => (
+  <Form.Item key={field} label={label} {...layout}>
+    {form.getFieldDecorator(field, {
+      initialValue: item ? item[field].split(',') : undefined,
+      rules: getRules(rules, label),
+    })(
+      <Select mode="multiple" placeholder={placeholder || label} {...rest}>
+        {options.map(option => <Select.Option value={option.id} key={option.id}>{option.name}</Select.Option>)}
+      </Select>
+    )}
+  </Form.Item>
+);
+TagSelect.defaultProps = { layout };
+export default TagSelect;
