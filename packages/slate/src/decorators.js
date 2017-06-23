3e7a8ad77308dbcaea8104bd0da0b475c6ff84e5
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-23 15:37:26 +0200

    cleanup slate

--- packages/slate/src/decorators.js
+++ packages/slate/src/decorators.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import useBlockBase from '../block-decorators/base';
+import useBlockBase from './block-decorators/base';
 import { createComponent } from 'olymp-fela';
 
 export default WrappedComponent =>
