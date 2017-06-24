903b1fce4e30638e174701a07b0d243539bd3721
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-24 14:54:39 +0200

    padding in h1/h2-blocks

--- packages/cms-gz/src/blocks/heading.js
+++ packages/cms-gz/src/blocks/heading.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { H1 as Heading1, H2 as Heading2 } from '../components';
 
 export const H1 = {
+  key: 'GZK.Template.H1BLock',
   label: 'Überschrift groß',
   category: 'Template',
   editable: true,
@@ -9,9 +10,13 @@ export const H1 = {
     (<Heading1 {...attributes}>
       {children}
     </Heading1>),
+  styles: ({ theme }) => ({
+    paddingY: theme.space2,
+  }),
 };
 
 export const H2 = {
+  key: 'GZK.Template.H2BLock',
   label: 'Überschrift klein',
   category: 'Template',
   editable: true,
@@ -19,4 +24,7 @@ export const H2 = {
     (<Heading2 {...attributes}>
       {children}
     </Heading2>),
+  styles: ({ theme }) => ({
+    paddingY: theme.space2,
+  }),
 };
