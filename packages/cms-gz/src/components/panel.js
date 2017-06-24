3e067e68a6196c2673ee1635568a86890191f86e
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-24 12:14:35 +0200

    panel improvements

--- packages/cms-gz/src/components/panel.js
+++ packages/cms-gz/src/components/panel.js
@@ -2,37 +2,38 @@ import React from 'react';
 import { Grid, createComponent, fade } from 'olymp-fela';
 import { H1 } from './heading';
 
-export default createComponent(
+export const Content = createComponent(
   ({
     theme,
     background = theme.dark5,
     color = theme.dark2,
-    padding = '1.33rem',
+    padding = theme.space3,
   }) => ({
+    backgroundColor: background,
+    color,
+    padding,
+    borderBottomRightRadius: 90,
+    // display: 'flex',
+    hyphens: 'auto',
+    flex: '1 1',
+    minHeight: 90,
+    '> iframe': {
+      display: 'block',
+      borderBottomRightRadius: 90,
+    },
+  }),
+  'div',
+  ({ padding, background, color, ...p }) => Object.keys(p)
+);
+
+export default createComponent(
+  ({ theme, background = theme.dark5 }) => ({
     width: '100%',
     paddingTop: theme.space3,
     paddingX: theme.space3,
     position: 'relative',
-    textAlign: 'justify',
     display: 'flex',
     flexDirection: 'column',
-    '> h2': {
-      marginBottom: theme.space1,
-    },
-    '> div': {
-      backgroundColor: background,
-      color,
-      padding,
-      borderBottomRightRadius: 100,
-      // display: 'flex',
-      hyphens: 'auto',
-      flex: '1 1',
-      minHeight: 90,
-      '> iframe': {
-        display: 'block',
-        borderBottomRightRadius: 100,
-      },
-    },
     onAfter: {
       content: '""',
       position: 'absolute',
@@ -41,7 +42,10 @@ export default createComponent(
       backgroundColor: theme.dark4,
       bottom: 0,
       borderTopLeftRadius: 30,
-      right: theme.space2,
+      right: theme.space3,
+    },
+    '> h2': {
+      marginBottom: theme.space1,
     },
     onHover: {
       onAfter: {
@@ -59,7 +63,9 @@ export default createComponent(
   ({ title, children, padding, background, color, bordered, ...rest }) =>
     (<Grid.Item mini={12} {...rest}>
       <H1 color={background} bordered={bordered}>{title}</H1>
-      <div>{children}</div>
+      <Content padding={padding} background={background} color={color}>
+        {children}
+      </Content>
     </Grid.Item>),
   p => Object.keys(p)
 );
