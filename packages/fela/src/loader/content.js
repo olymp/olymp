5144770ff3680a0e8a707231cb6d8a2446ac0c1c
Benjamin Kniffler <bkniffler@me.com>
2017-06-27 17:41:35 +0100

    content-loader cleanup

--- packages/fela/src/loader/content.js
+++ packages/fela/src/loader/content.js
@@ -1,19 +1,5 @@
 import React, { Children } from 'react';
 import { createComponent } from 'react-fela';
-import Container from '../container';
-import Grid from '../grid';
-
-// const loaderSchema2 = `450f,[c[m5[100,200,400]]g]`
-const Panel = createComponent(
-  ({ height, width, pad, theme }) => ({
-    height,
-    width,
-    paddingRight: pad ? theme.space3 : 0,
-    marginBottom: theme.space3,
-  }),
-  'div',
-  []
-);
 
 export const ContentLoaderStyles = {
   animationDuration: '1s',
@@ -33,7 +19,7 @@ export const ContentLoaderStyles = {
   },
 };
 
-const ContentLoader = createComponent(
+export default createComponent(
   ({ height, width }) => ({
     height: height || '100%',
     // minHeight: 96,
@@ -51,58 +37,3 @@ const ContentLoader = createComponent(
   },
   props => Object.keys(props)
 );
-
-export const SchemaLoader = ({ schema, isLoading, children }) => {
-  if (isLoading) {
-    const components = schema.map((item, i) =>
-      <SchemaLoaderItem key={i} {...item} />
-    );
-    return <div>{components}</div>;
-  }
-  if (children) {
-    return Children.only(children);
-  }
-  return null;
-};
-
-const SchemaLoaderItem = ({ height, width, grid }) => {
-  let inner;
-  if (grid) {
-    inner = (
-      <Grid height="100%">
-        {grid.map(({ children, ...item }, i) =>
-          (<Grid.Item key={i} height="100%" {...item}>
-            <Panel height="100%" width="100%" pad>
-              {children
-                ? children.map((child, i) =>
-                  (<Panel key={i}>
-                    <ContentLoader isLoading {...child} />
-                  </Panel>)
-                  )
-                : <ContentLoader isLoading />}
-            </Panel>
-          </Grid.Item>)
-        )}
-      </Grid>
-    );
-  } else {
-    inner = <ContentLoader isLoading />;
-  }
-
-  if (width === 'container') {
-    return (
-      <Container height={height}>
-        <Panel height="100%" width="100%">
-          {inner}
-        </Panel>
-      </Container>
-    );
-  }
-  return (
-    <Panel height={height} width={width}>
-      {inner}
-    </Panel>
-  );
-};
-
-export default ContentLoader;
