4d9b4bcdba7dcef77a09e762cbb54cd9818fd54b
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-28 19:40:39 +0200

    optimize image-block, float-option

--- packages/pages/src/blocks/image/block.js
+++ packages/pages/src/blocks/image/block.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import { createComponent } from 'olymp-fela';
 import { createBlockList } from 'olymp-slate';
 import Image from './image';
 import Label from './label';
-
-const component = createComponent(
-  ({ float }) => ({
-    width: '100%',
-    float,
-    position: 'relative',
-  }),
-  ({ className, attributes, children }) =>
-    (<div className={className} {...attributes}>
-      {children}
-    </div>),
-  p => Object.keys(p)
-);
 
 export default {
   key: 'Pages.Media.ImageBlock',
@@ -23,5 +9,6 @@ export default {
   label: 'Bild mit Text',
   category: 'Medien',
   editable: true,
-  component,
+  component: ({ className, children, attributes }) =>
+    <div className={className} {...attributes}>{children}</div>,
 };
--- packages/pages/src/blocks/image/image.js
+++ packages/pages/src/blocks/image/image.js
@@ -9,9 +9,16 @@ export default {
     (<Image
       className={className}
       onClick={setActive}
-      width="100%"
-      value={getData('value', { url: 'https://lorempixel.com/1000/300/cats/' })}
+      width={getData('float', 'none') === 'none' ? '100%' : '25%'}
+      value={getData('value', {
+        url: 'https://lorempixel.com/1000/300/cats/',
+        width: 1000,
+        height: 300,
+      })}
     />),
+  styles: ({ getData }) => ({
+    float: getData('float', 'none'),
+  }),
   actions: [
     {
       component: ({ setData, getData, ...p }) =>
@@ -22,6 +29,22 @@ export default {
           multi={false}
         />),
       toggle: () => {},
+    },
+    {
+      label: 'Links',
+      active: ({ getData }) => getData('float', 'none') === 'left',
+      toggle: ({ setData, getData }) =>
+        setData({
+          float: getData('float', 'none') === 'left' ? 'none' : 'left',
+        }),
+    },
+    {
+      label: 'Rechts',
+      active: ({ getData }) => getData('float', 'none') === 'right',
+      toggle: ({ setData, getData }) =>
+        setData({
+          float: getData('float', 'none') === 'right' ? 'none' : 'right',
+        }),
     },
   ],
 };
