0a81560fc0484dd2e37a2734e149da069f7cba10
Philipp Achenbach <github@philipp-achenbach.de>
2017-06-28 16:57:27 +0200

    fix error in storybook

--- packages/fela/.storybook/config.js
+++ packages/fela/.storybook/config.js
@@ -3,6 +3,15 @@ import PropVal from '@storybook/addon-info/dist/components/PropVal';
 import { configure, setAddon } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
 
+// temp fix PropVal.propTypes
+// https://github.com/storybooks/storybook/issues/1305
+PropVal.propTypes = {
+  ...PropVal.propTypes,
+  maxPropObjectKeys: PropTypes.number,
+  maxPropArrayLength: PropTypes.number,
+  maxPropStringLength: PropTypes.number,
+};
+
 setAddon(infoAddon);
 configure(() => require('../src/navbar/.stories'), module);
 configure(() => require('../src/.stories'), module);
