cf23f7e9e70019d00e5fd507a2615f035d22127c
Philipp Achenbach <github@philipp-achenbach.de>
2017-07-03 18:04:34 +0200

    size and alignment in images

--- packages/pages/src/blocks/image/image.js
+++ packages/pages/src/blocks/image/image.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Image, SimpleImageEdit } from 'olymp-cloudinary';
+import { FaAlignLeft, FaAlignRight, FaPlus, FaMinus } from 'olymp-icons';
 
 export default {
   key: 'Pages.Media.ImageBlock.Image',
@@ -9,15 +10,21 @@ export default {
     (<Image
       className={className}
       onClick={setActive}
-      width={getData('float', 'none') === 'none' ? '100%' : '25%'}
+      width={`${100 / getData('size', 4)}%`}
       value={getData('value', {
-        url: 'https://lorempixel.com/1000/300/cats/',
-        width: 1000,
+        url: 'https://lorempixel.com/400/300/cats/',
+        width: 400,
         height: 300,
       })}
     />),
-  styles: ({ getData }) => ({
+  styles: ({ theme, getData }) => ({
     float: getData('float', 'none'),
+    margin: getData('float', 'none') === 'none' && '0 auto',
+    marginTop: getData('float', 'none') === 'none' && theme.space3,
+    marginBottom: theme.space3,
+    marginLeft: getData('float', 'none') !== 'left' && theme.space3,
+    marginRight: getData('float', 'none') !== 'right' && theme.space3,
+    zIndex: 1,
   }),
   actions: [
     {
@@ -31,7 +38,7 @@ export default {
       toggle: () => {},
     },
     {
-      label: 'Links',
+      label: <FaAlignLeft />,
       active: ({ getData }) => getData('float', 'none') === 'left',
       toggle: ({ setData, getData }) =>
         setData({
@@ -39,12 +46,30 @@ export default {
         }),
     },
     {
-      label: 'Rechts',
+      label: <FaAlignRight />,
       active: ({ getData }) => getData('float', 'none') === 'right',
       toggle: ({ setData, getData }) =>
         setData({
           float: getData('float', 'none') === 'right' ? 'none' : 'right',
         }),
+    },
+    {
+      label: <FaPlus />,
+      toggle: ({ setData, getData }) => {
+        const size = getData('size', 4);
+        setData({
+          size: size > 1 ? size - 1 : 1,
+        });
+      },
+    },
+    {
+      label: <FaMinus />,
+      toggle: ({ setData, getData }) => {
+        const size = getData('size', 4);
+        setData({
+          size: size < 8 ? size + 1 : 8,
+        });
+      },
     },
   ],
 };
