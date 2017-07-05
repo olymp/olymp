c66a39ce0d033e04d77de3087f58b356fbcd0191
Benjamin Kniffler <bkniffler@me.com>
2017-07-05 18:35:14 +0200

    image edit placeholder

--- packages/cloudinary/src/image-edit/image-edit.js
+++ packages/cloudinary/src/image-edit/image-edit.js
@@ -3,5 +3,9 @@ import createEdit from './edit';
 import { Image } from '../components';
 
 export default createEdit(
-  v => <Image value={v[0]} />
+  v => <Image value={v[0] || {
+    url: 'https://lorempixel.com/1000/300/cats/',
+    width: 1000,
+    height: 300,
+  }} />
 );
