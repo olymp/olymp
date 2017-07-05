2b0155440ba7c7395497a0c92b740202d7e0b137
Philipp Achenbach <github@philipp-achenbach.de>
2017-07-05 12:39:30 +0200

    cursor & hover on lighbox-image

--- packages/cloudinary/src/lightbox/image.js
+++ packages/cloudinary/src/lightbox/image.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'olymp';
+import { createComponent } from 'olymp-fela';
 import Image, { cloudinaryUrl } from '../components/image';
+
+const Img = createComponent(
+  ({ theme }) => ({
+    cursor: 'pointer',
+    onHover: {
+      opacity: 0.85,
+    },
+  }),
+  p => <Image {...p} />,
+  p => Object.keys(p)
+);
 
 @withRouter
 export default class Lightbox extends Component {
@@ -14,7 +26,9 @@ export default class Lightbox extends Component {
   componentWillMount() {
     const { value } = this.props;
 
-    if (value) { this.add(value); }
+    if (value) {
+      this.add(value);
+    }
   }
 
   componentWillReceiveProps({ value }) {
@@ -86,6 +100,6 @@ export default class Lightbox extends Component {
       return <div />;
     }
 
-    return <Image onClick={this.onClick} value={value} {...rest} />;
+    return <Img onClick={this.onClick} value={value} {...rest} />;
   }
 }
