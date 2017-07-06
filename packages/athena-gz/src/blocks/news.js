403a0a885fa5c24759b7e7385d9ef56b5323fa36
Philipp Achenbach <github@philipp-achenbach.de>
2017-07-06 11:52:29 +0200

    small fixes gz news

--- packages/athena-gz/src/blocks/news.js
+++ packages/athena-gz/src/blocks/news.js
@@ -42,11 +42,8 @@ const getItems = (items, filter, title) =>
   );
 
 class Item extends Component {
-  state = { open: false };
-
   render() {
-    const { art, date, id, name, extrakt, slug, text } = this.props;
-    const { open } = this.state;
+    const { art, date, id, name, extrakt, onClick, text, open } = this.props;
 
     const bild = this.props.bild || {
       url:
@@ -60,7 +57,6 @@ class Item extends Component {
         id={id}
         // color={item.org.farbe}
         subtitle={`${art} vom ${moment(date).format('DD.MM.YYYY')}`}
-        key={id}
       >
         <Img value={bild} width={100} avatar />
         <Content>
@@ -74,14 +70,14 @@ class Item extends Component {
                 {extrakt}
               </p>}
           </div>
-          <More open={open} onClick={() => this.setState({ open: !open })} />
+          {!!text && <More open={open} onClick={onClick} />}
         </Content>
       </Panel>
     );
   }
 }
 
-const component = graphql(
+@graphql(
   gql`
     query terminList {
       items: terminList(sort: { date: DESC }) {
@@ -122,57 +118,79 @@ const component = graphql(
       items: data.items || [],
     }),
   }
-)(({ attributes, items, isLoading }) =>
-  (<SchemaLoader isLoading={isLoading} schema={loaderSchema}>
-    <Container {...attributes}>
-      <Grid>
-        <Grid.Item medium={7} paddingMedium="0 0 0 0.5rem">
-          {items.map(item => <Item {...item} />)}
-        </Grid.Item>
-        <Column
-          medium={4}
-          offsetMedium={1}
-          offsetLarge={1}
-          offsetHuge={1}
-          paddingMini="1rem 1rem 0 1rem"
-          paddingMedium="0 1rem"
-        >
-          <H2 right>Vorträge & Veranstaltungen</H2>
-          <ul>
-            {getItems(
-              items,
-              item => item.art === 'VORTRAG' || item.art === 'VERANSTALTUNG',
-              item => `${moment(item.date).format('DD. MMMM YYYY, HH:mm')} Uhr`
-            )}
-          </ul>
+)
+class News extends Component {
+  state = { open: undefined };
 
-          <H2 right>Publikationen</H2>
-          <ul>
-            {getItems(
-              items,
-              item => item.art === 'PUBLIKATION',
-              item => moment(item.date).format('DD. MMMM YYYY')
-            )}
-          </ul>
+  render() {
+    const { attributes, items, isLoading } = this.props;
+    const { open } = this.state;
 
-          <H2 right>Presse</H2>
-          <ul>
-            {getItems(
-              items,
-              item => item.art === 'PRESSE',
-              item => moment(item.date).format('DD. MMMM YYYY')
-            )}
-          </ul>
-        </Column>
-      </Grid>
-    </Container>
-  </SchemaLoader>)
-);
+    return (
+      <SchemaLoader isLoading={isLoading} schema={loaderSchema}>
+        <Container {...attributes}>
+          <Grid>
+            <Grid.Item medium={7} paddingMedium="0 0 0 0.5rem">
+              {items.map(item =>
+                (<Item
+                  {...item}
+                  open={open === item.id}
+                  onClick={() =>
+                    this.setState({
+                      open: open === item.id ? undefined : item.id,
+                    })}
+                  key={item.id}
+                />)
+              )}
+            </Grid.Item>
+            <Column
+              medium={4}
+              offsetMedium={1}
+              offsetLarge={1}
+              offsetHuge={1}
+              paddingMini="1rem 1rem 0 1rem"
+              paddingMedium="0 1rem"
+            >
+              <H2 right>Vorträge & Veranstaltungen</H2>
+              <ul>
+                {getItems(
+                  items,
+                  item =>
+                    item.art === 'VORTRAG' || item.art === 'VERANSTALTUNG',
+                  item =>
+                    `${moment(item.date).format('DD. MMMM YYYY, HH:mm')} Uhr`
+                )}
+              </ul>
+
+              <H2 right>Publikationen</H2>
+              <ul>
+                {getItems(
+                  items,
+                  item => item.art === 'PUBLIKATION',
+                  item => moment(item.date).format('DD. MMMM YYYY')
+                )}
+              </ul>
+
+              <H2 right>Presse</H2>
+              <ul>
+                {getItems(
+                  items,
+                  item => item.art === 'PRESSE',
+                  item => moment(item.date).format('DD. MMMM YYYY')
+                )}
+              </ul>
+            </Column>
+          </Grid>
+        </Container>
+      </SchemaLoader>
+    );
+  }
+}
 
 export default {
   key: 'GZK.Collections.NewsBlock',
   label: 'Neuigkeiten',
   category: 'Collections',
   editable: false,
-  component,
+  component: News,
 };
