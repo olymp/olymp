ece6bd0d3325f7453a0e5be7a164f9454ed03ff9
Philipp Achenbach <github@philipp-achenbach.de>
2017-07-06 11:42:09 +0200

    old toolbar

--- packages/slate/src/toolbar.js
+++ packages/slate/src/toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Portal from 'react-portal';
 import { Gateway } from 'react-gateway';
-import { Tooltip } from 'antd';
+import { Menu, Tooltip, Icon } from 'antd';
 import { createComponent } from 'react-fela';
 
 export const Button = ({ onMouseDown, tooltip, children }) =>
@@ -9,6 +10,14 @@ export const Button = ({ onMouseDown, tooltip, children }) =>
       {children}
     </Tooltip>
   </div>);
+
+const Close = createComponent(
+  ({ theme }) => ({
+    float: 'right !important',
+  }),
+  p => <Menu.Item {...p} />,
+  p => Object.keys(p)
+);
 
 export default createComponent(
   ({ theme }) => ({
@@ -20,7 +29,7 @@ export default createComponent(
     paddingX: theme.space2,
   }),
   (props) => {
-    const { isOpened, children } = props;
+    const { isOpened, className, children } = props;
 
     if (!isOpened) {
       return <div />;
@@ -30,6 +39,18 @@ export default createComponent(
       <Gateway into="toolbar">
         {children}
       </Gateway>
+    );
+
+    // Old one, but pimped!
+    return (
+      <Portal isOpened={!!isOpened}>
+        <Menu className={className} mode="horizontal" theme="dark">
+          {children}
+          <Close>
+            <Icon type="close" />
+          </Close>
+        </Menu>
+      </Portal>
     );
   },
   p => Object.keys(p)
