181140b2ce6387a63ca6fe5a614b696ddcbf0a4d
Benjamin Kniffler <bkniffler@me.com>
2017-07-08 15:19:47 +0200

    omit & traverse

--- packages/core/src/omit.js
+++ packages/core/src/omit.js
@@ -1,15 +1,15 @@
 import { isObject, isArray } from 'lodash';
 
-const omit = (obj) => {
+const omit = (obj, keyToOmit = '__typename') => {
   if (!isObject(obj)) {
     return obj;
   }
   const newObj = Object.keys(obj).reduce((store, key) => {
     if (isArray(obj[key])) {
-      store[key] = obj[key].map(omit);
+      store[key] = obj[key].map(omit, keyToOmit);
     } else if (isObject(obj[key])) {
-      store[key] = omit(obj[key]);
-    } else if (key !== '__typename') {
+      store[key] = omit(obj[key], keyToOmit);
+    } else if (key !== keyToOmit) {
       store[key] = obj[key];
     }
     return store;
