376564432474693b8e753c49a16145680c2cf10f
Benjamin Kniffler <bkniffler@me.com>
2017-07-09 11:54:46 +0200

    server

--- packages/athena/server/index.js
+++ packages/athena/server/index.js
@@ -11,125 +11,148 @@ import { get } from 'lodash';
 
 const {
   APP,
-  URL,
-  MONGODB_URI,
-  POSTMARK_KEY,
-  POSTMARK_FROM,
-  CLOUDINARY_URI,
-  // FILESTACK_KEY,
-  AUTH_SECRET,
-  NODE_ENV,
-  GOOGLE_MAPS_KEY,
-  GOOGLE_CLIENT_EMAIL,
-  GOOGLE_PRIVATE_KEY,
+    URL,
+    MONGODB_URI,
+    POSTMARK_KEY,
+    POSTMARK_FROM,
+    CLOUDINARY_URI,
+    // FILESTACK_KEY,
+    AUTH_SECRET,
+    NODE_ENV,
+    GOOGLE_MAPS_KEY,
+    GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY,
 } = process.env;
 
 export default (server, options) => {
-  const monk = createMonk(MONGODB_URI);
+    const monk = createMonk(MONGODB_URI);
 
-  const schema = createSchema({ directives });
-  const modules = {
-    helloWorld: {
-      queries: `
+    const schema = createSchema({ directives });
+    const modules = {
+        helloWorld: {
+            queries: `
       helloWorld: String
     `,
-      resolvers: {
-        queries: {
-          helloWorld: () => 'Hello World!',
+            resolvers: {
+                queries: {
+                    helloWorld: () => 'Hello World!',
+                },
+            },
         },
-      },
-    },
-    ...get(options, 'modules', {}),
-    ...colModules,
-  };
-  const mail = createMail(POSTMARK_KEY, {
-    from: POSTMARK_FROM,
-    url: URL,
-  });
+        ...get(options, 'modules', {}),
+        ...colModules,
+    };
+    const mail = createMail(POSTMARK_KEY, {
+        from: POSTMARK_FROM,
+        url: URL,
+    });
 
-  const authEngine = createAuthEngine({ monk, mail, secret: AUTH_SECRET });
-  server.use(authCache(authEngine));
+    const authEngine = createAuthEngine({ monk, mail, secret: AUTH_SECRET });
+    server.use(authCache(authEngine));
 
-  let cachedApp = null;
-  server.use((req, res, next) => {
-    req.mail = mail;
-    req.monk = monk;
-    req.schema = schema;
-    req.authEngine = authEngine;
-    req.app = cachedApp;
-    next();
-  });
+    let cachedApp = null;
+    server.use((req, res, next) => {
+        req.mail = mail;
+        req.monk = monk;
+        req.schema = schema;
+        req.authEngine = authEngine;
+        req.app = cachedApp;
+        next();
+    });
 
-  modules.auth = authGraphQL(options.auth);
-  modules.pages = pagesGraphQL();
-  modules.cloudinary = cloudinaryGraphQL(CLOUDINARY_URI);
-  modules.google = googleGraphQL(
-    GOOGLE_MAPS_KEY,
-    GOOGLE_CLIENT_EMAIL,
-    GOOGLE_PRIVATE_KEY
-  );
-  /*
-  createSitemap(schema, options.sitemap);
-  googleGraphQL(schema, GM_KEY, options.google);
-  pagesGraphQL(schema, options.pages);
-  cloudinaryGraphQL(schema, CLOUDINARY_URI, options.cloudinary);
+    modules.auth = authGraphQL(options.auth);
+    modules.pages = pagesGraphQL();
+    modules.cloudinary = cloudinaryGraphQL(CLOUDINARY_URI);
+    modules.google = googleGraphQL(
+        GOOGLE_MAPS_KEY,
+        GOOGLE_CLIENT_EMAIL,
+        GOOGLE_PRIVATE_KEY
+    );
+    /*
+    createSitemap(schema, options.sitemap);
+    googleGraphQL(schema, GM_KEY, options.google);
+    pagesGraphQL(schema, options.pages);
+    cloudinaryGraphQL(schema, CLOUDINARY_URI, options.cloudinary);
 
-  if (options.schemas) {
-    options.schemas({ schema, mail, authEngine, server, monk });
-  }
-
-  let cachedApp = null;
-  // Add GraphQL API
-  server.post('/graphql', (req, res, next) => {
-    if (cachedApp) {
-      return schema.express(req, res, next);
+    if (options.schemas) {
+      options.schemas({ schema, mail, authEngine, server, monk });
     }
-    monk
-      .collection('apps')
-      .findOne({ id: APP })
-      .then((app) => {
+
+    let cachedApp = null;
+    // Add GraphQL API
+    server.post('/graphql', (req, res, next) => {
+      if (cachedApp) {
+        return schema.express(req, res, next);
+      }
+      monk
+        .collection('apps')
+        .findOne({ id: APP })
+        .then((app) => {
+          if (!app) {
+            throw new Error('App not found');
+          }
+          cachedApp = app;
+          // attach schemata
+          // schema.addSchema();
+          schema.express(req, res, next);
+        })
+        .catch(err => next(err));
+    });*/
+    server.post('/graphql', schema.express);
+    if (NODE_ENV !== 'production') {
+        server.get('/graphql', schema.graphi);
+    }
+
+    monk.collection('app').findOne({ id: APP }).then((app) => {
         if (!app) {
-          throw new Error('App not found');
+            throw new Error('App not found');
         }
         cachedApp = app;
-        // attach schemata
-        // schema.addSchema();
-        schema.express(req, res, next);
-      })
-      .catch(err => next(err));
-  });*/
-  server.post('/graphql', schema.express);
-  if (NODE_ENV !== 'production') {
-    server.get('/graphql', schema.graphi);
-  }
+        schema.apply(modules);
+    });
 
-  monk.collection('app').findOne({ id: APP }).then((app) => {
-    if (!app) {
-      throw new Error('App not found');
-    }
-    cachedApp = app;
-    schema.apply(modules);
-  });
+    const shortId = require('shortid');
+    /*monk.collection('item').find({ _type: 'termin' }).then((items) => {
+        // monk.collection('item').find({ _type: 'termin' }).then((items) => {
+        items.forEach((item) => {
+            item.extract = item.extrakt;
+            delete item.extrakt;
+            item.image = item.bild;
+            delete item.image;
+            delete item._id;
+            item.id = shortId.generate();
+            if (item.art && item.art.indexOf('V') === 0) {
+                monk.collection('item').insert({ ...item, _type: 'event' });
+            } else {
+                monk.collection('item').insert({ ...item, _type: 'news' });
+            }
+        });
+    });*/
+    /*monk.collection('item').find({ _type: 'termin' }).then((items) => {
+      items.forEach((item) => {
+        item.extract = item.extract;
+        delete item.extract;
+        item.image = item.image;
+        delete item.image;
+        if (item.type && item.type.indexOf('V') === 0) {
+          monk.collection('item').update({ ...item, _type: 'event' });
+        } else if (item.type && item.type.indexOf('V') === 0) {
+          monk.collection('item').update({ ...item, _type: 'news' });
+        }
+      });
+    });
 
-  /*monk.collection('item').find({ _type: 'artikel' }).then((items) => {
-    items.forEach((item) => {
-      if (!item.extrakt) item.extrakt = 'Extrakt';
-      if (!item.date) item.date = +new Date();
-      if (!item.slug) item.slug = '/' + item.date;
-      if (!item.name) item.name = item.date;
-      monk.collection('item').update({ id: item.id, _type: item._type }, { ...item });
+    */
+    /* monk.collection('page').find({}).then((items) => {
+      items.forEach((item) => {
+        monk.collection('item').insert({ ...item, _type: 'page', _appId: 'gzk' });
+      });
     });
-  });*/
-  /* monk.collection('page').find({}).then((items) => {
-    items.forEach((item) => {
-      monk.collection('item').insert({ ...item, _type: 'page', _appId: 'gzk' });
-    });
-  });
-  monk.collection('file').find({}).then((items) => {
-    items.forEach((item) => {
-      monk.collection('item').insert({ ...item, _type: 'file', _appId: 'gzk' });
-    });
-  });*/
+    monk.collection('file').find({}).then((items) => {
+      items.forEach((item) => {
+        monk.collection('item').insert({ ...item, _type: 'file', _appId: 'gzk' });
+      });
+    });*/
 };
 
 /* if (mail) mail({ to: 'bkniffler@me.com', subject: 'Hello!', markdown: `
