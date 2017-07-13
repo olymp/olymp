a2c464133a8ee26546a5a923cc12e209362fd463
Benjamin Kniffler <bkniffler@me.com>
2017-07-13 14:15:12 +0200

    locale

---
+++ packages/locale/de.js
@@ -0,0 +1,30 @@
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+import enUS from 'antd/lib/locale-provider/de_DE';
+import { withLocale as withLocale2 } from 'olymp-utils';
+import dateLocale from 'date-fns/locale/de';
+export default function (LANG) {
+    if (LANG === void 0) { LANG = {}; }
+    return function (WrappedComponent) { return function (props) {
+        return locale = { enUS: enUS } >
+            locale;
+    }; };
+};
+{
+    {
+        LANG, ;
+        dateLocale;
+    }
+}
+ >
+    __assign({}, props) /  >
+    /LocaleProvider>
+    < /AntLocaleProvider>);;
+export var withLocale = withLocale2;
+//# sourceMappingURL=de.js.map
