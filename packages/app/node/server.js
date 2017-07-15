6d6b2b3a247aaa7a62109e152e6e76122df65d05
Benjamin Kniffler <bkniffler@me.com>
2017-07-15 09:33:53 +0200

    deps

--- packages/app/node/server.js
+++ packages/app/node/server.js
@@ -9,7 +9,10 @@ import { StaticRouter } from 'olymp-router';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 import { ApolloProvider, getDataFromTree } from 'react-apollo';
 import { ApolloClient, createNetworkInterface } from 'apollo-client';
-import { AsyncComponentProvider, createAsyncContext, } from 'react-async-component';
+import {
+  AsyncComponentProvider,
+  createAsyncContext,
+} from 'react-async-component';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import { Provider } from 'react-fela';
 import Helmet from 'react-helmet';
@@ -37,42 +40,51 @@ var port = parseInt(process.env.PORT, 10);
 var devPort = parseInt(process.env.DEV_PORT, 10);
 var clientAssetsPath = path.resolve(__dirname, '..', 'web', 'assets.json');
 var clientAssets = fs.existsSync(clientAssetsPath)
-    ? JSON.parse(fs.readFileSync(clientAssetsPath))
-    : null;
+  ? JSON.parse(fs.readFileSync(clientAssetsPath))
+  : null;
 var app = express();
 app.use(helmet());
 if (process.env.NODE_ENV === 'production' && process.env.URL) {
-    app.use(sslRedirect());
+  app.use(sslRedirect());
 }
 app.use(compression());
 app.use(useragent.express());
 app.use(express.static(path.resolve(process.cwd(), 'public')));
 app.use(express.static(path.resolve(process.cwd(), 'build', 'web')));
-app.use(express.static(path.resolve(process.cwd(), 'node_modules', 'olymp', 'public')));
+app.use(
+  express.static(
+    path.resolve(process.cwd(), 'node_modules', 'olymp-app', 'public')
+  )
+);
 app.use(bodyparser.json());
-app.use(function (req, res, next) {
-    if (req.subdomains &&
-        req.subdomains.length === 1 &&
-        req.subdomains[0] === 'amp') {
-        req.isAmp = true;
-    }
-    else if (req.query.amp !== undefined) {
-        req.isAmp = true;
-    }
-    next();
+app.use(function(req, res, next) {
+  if (
+    req.subdomains &&
+    req.subdomains.length === 1 &&
+    req.subdomains[0] === 'amp'
+  ) {
+    req.isAmp = true;
+  } else if (req.query.amp !== undefined) {
+    req.isAmp = true;
+  }
+  next();
 });
-var trust = process.env.TRUST_PROXY !== undefined ? parseInt(process.env.TRUST_PROXY) : 2;
-var secure = process.env.COOKIE_SECURE !== undefined
-    ? "" + process.env.COOKIE_SECURE === 'true'
+var trust =
+  process.env.TRUST_PROXY !== undefined ? parseInt(process.env.TRUST_PROXY) : 2;
+var secure =
+  process.env.COOKIE_SECURE !== undefined
+    ? '' + process.env.COOKIE_SECURE === 'true'
     : isProd;
-var domain = process.env.URL !== undefined ? process.env.URL.split('/')[2] : undefined;
+var domain =
+  process.env.URL !== undefined ? process.env.URL.split('/')[2] : undefined;
 if (isProd) {
-    app.set('trust proxy', trust);
+  app.set('trust proxy', trust);
 }
-app.use(session({
+app.use(
+  session({
     store: process.env.REDIS_URL
-        ? new RedisStore({ url: process.env.REDIS_URL, logErrors: true })
-        : undefined,
+      ? new RedisStore({ url: process.env.REDIS_URL, logErrors: true })
+      : undefined,
     resave: false,
     saveUninitialized: true,
     httpOnly: true,
@@ -80,92 +92,121 @@ app.use(session({
     domain: domain,
     secret: process.env.SESSION_SECRET || 'keyboard cat',
     cookie: {
-        secure: secure,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: secure,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
     },
-}));
+  })
+);
 try {
-    var server = require('@root/server');
-    if (server.default) {
-        server.default(app);
-    }
-    else {
-        server(app);
-    }
+  var server = require('@root/server');
+  if (server.default) {
+    server.default(app);
+  } else {
+    server(app);
+  }
+} catch (err) {
+  console.log(
+    'No server.js or server/index.js file found, using default settings',
+    err
+  );
 }
-catch (err) {
-    console.log('No server.js or server/index.js file found, using default settings', err);
-}
-app.get('*', function (req, res) {
-    var networkInterface = createNetworkInterface({
-        uri: process.env.GRAPHQL_URL || "http://localhost:" + port + "/graphql",
-        opts: {
-            credentials: 'same-origin',
-            headers: req.headers,
-        },
-    });
-    var client = new ApolloClient({
-        networkInterface: networkInterface,
-        dataIdFromObject: function (o) { return o.id; },
-        ssrMode: true,
-    });
-    var ua = new UAParser(req.headers['user-agent']);
-    var renderer = createFela(ua);
-    var store = createStore(combineReducers({
-        apollo: client.reducer(),
-    }), {}, compose(applyMiddleware(client.middleware())));
-    var asyncContext = createAsyncContext();
-    if (typeof init !== undefined && init) {
-        init({ renderer: renderer, client: client, store: store });
-    }
-    var context = {};
-    var reactApp = (React.createElement(AsyncComponentProvider, { asyncContext: asyncContext },
-        React.createElement(ApolloProvider, { store: store, client: client },
-            React.createElement(StaticRouter, { location: req.url, context: context },
-                React.createElement(Provider, { renderer: renderer },
-                    React.createElement(GatewayProvider, null,
-                        React.createElement(UAProvider, { ua: ua },
-                            React.createElement(AmpProvider, { amp: req.isAmp },
-                                React.createElement(App, null)))))))));
-    return asyncBootstrapper(reactApp)
-        .then(function () { return getDataFromTree(reactApp); })
-        .then(function () {
-        var reactAppString = req.isAmp
-            ? renderToStaticMarkup(reactApp)
-            : renderToString(reactApp);
-        var scripts = req.isAmp
-            ? []
-            : [
-                isProd
-                    ? "" + clientAssets.main.js
-                    : process.env.DEV_URL + "/main.js",
-            ];
-        var styles = req.isAmp
-            ? []
-            : isProd ? ["" + clientAssets.main.css] : [];
-        var cssMarkup = renderer.renderToString();
-        var asyncState = asyncContext.getState();
-        var html = (req.isAmp ? amp : template)({
-            root: reactAppString,
-            scripts: scripts,
-            styles: styles,
-            cssMarkup: cssMarkup,
-            helmet: Helmet.rewind(),
-            initialState: { apollo: client.getInitialState() },
-            asyncState: asyncState,
-            gaTrackingId: process.env.GA_TRACKING_ID,
-        });
-        if (context.url) {
-            res.status(301).setHeader('Location', context.url);
-            res.end();
-            return;
-        }
-        res.status(context.missed ? 404 : 200);
-        res.send(html);
+app.get('*', function(req, res) {
+  var networkInterface = createNetworkInterface({
+    uri: process.env.GRAPHQL_URL || 'http://localhost:' + port + '/graphql',
+    opts: {
+      credentials: 'same-origin',
+      headers: req.headers,
+    },
+  });
+  var client = new ApolloClient({
+    networkInterface: networkInterface,
+    dataIdFromObject: function(o) {
+      return o.id;
+    },
+    ssrMode: true,
+  });
+  var ua = new UAParser(req.headers['user-agent']);
+  var renderer = createFela(ua);
+  var store = createStore(
+    combineReducers({
+      apollo: client.reducer(),
+    }),
+    {},
+    compose(applyMiddleware(client.middleware()))
+  );
+  var asyncContext = createAsyncContext();
+  if (typeof init !== undefined && init) {
+    init({ renderer: renderer, client: client, store: store });
+  }
+  var context = {};
+  var reactApp = React.createElement(
+    AsyncComponentProvider,
+    { asyncContext: asyncContext },
+    React.createElement(
+      ApolloProvider,
+      { store: store, client: client },
+      React.createElement(
+        StaticRouter,
+        { location: req.url, context: context },
+        React.createElement(
+          Provider,
+          { renderer: renderer },
+          React.createElement(
+            GatewayProvider,
+            null,
+            React.createElement(
+              UAProvider,
+              { ua: ua },
+              React.createElement(
+                AmpProvider,
+                { amp: req.isAmp },
+                React.createElement(App, null)
+              )
+            )
+          )
+        )
+      )
+    )
+  );
+  return asyncBootstrapper(reactApp)
+    .then(function() {
+      return getDataFromTree(reactApp);
     })
-        .catch(function (err) {
-        console.error(err);
-        res.status(500).send(err);
+    .then(function() {
+      var reactAppString = req.isAmp
+        ? renderToStaticMarkup(reactApp)
+        : renderToString(reactApp);
+      var scripts = req.isAmp
+        ? []
+        : [
+            isProd
+              ? '' + clientAssets.main.js
+              : process.env.DEV_URL + '/main.js',
+          ];
+      var styles = req.isAmp ? [] : isProd ? ['' + clientAssets.main.css] : [];
+      var cssMarkup = renderer.renderToString();
+      var asyncState = asyncContext.getState();
+      var html = (req.isAmp ? amp : template)({
+        root: reactAppString,
+        scripts: scripts,
+        styles: styles,
+        cssMarkup: cssMarkup,
+        helmet: Helmet.rewind(),
+        initialState: { apollo: client.getInitialState() },
+        asyncState: asyncState,
+        gaTrackingId: process.env.GA_TRACKING_ID,
+      });
+      if (context.url) {
+        res.status(301).setHeader('Location', context.url);
+        res.end();
+        return;
+      }
+      res.status(context.missed ? 404 : 200);
+      res.send(html);
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send(err);
     });
 });
 export default app;
