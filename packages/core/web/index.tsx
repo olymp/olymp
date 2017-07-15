4ea29097cdbeabf8b3b946f9d68db37c87b31b26
Benjamin Kniffler <bkniffler@me.com>
2017-07-15 12:52:31 +0200

    small

--- packages/core/web/index.tsx
+++ packages/core/web/index.tsx
@@ -1,11 +1,12 @@
-/* eslint-disable global-require */
-
 import React from 'react';
 import { render } from 'react-dom';
 import { AmpProvider, UAProvider, UAParser } from 'olymp-utils';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client';
-import { AsyncComponentProvider, createAsyncContext } from 'react-async-component';
+import {
+  AsyncComponentProvider,
+  createAsyncContext,
+} from 'react-async-component';
 import asyncBootstrapper from 'react-async-bootstrapper';
 import { createFela } from 'olymp-fela';
 import { Provider as FelaProvider } from 'react-fela';
@@ -59,9 +60,9 @@ if (process.env.NODE_ENV === 'production') {
 
 const networkInterface = createBatchingNetworkInterface({
   uri:
-  process.env.GRAPHQL_URL ||
-  (process.env.URL && `${process.env.URL}/graphql`) ||
-  '/graphql',
+    process.env.GRAPHQL_URL ||
+    (process.env.URL && `${process.env.URL}/graphql`) ||
+    '/graphql',
   batchInterval: 5,
   opts: {
     credentials: 'same-origin',
