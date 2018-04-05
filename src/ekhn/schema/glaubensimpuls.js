0fb9c7a6cfec0f3ed05c4da2ce28fed7d5c59a04
Philipp Achenbach <github@philipp-achenbach.de>
2018-04-05 14:07:43 +0200

    small fix

--- src/ekhn/schema/glaubensimpuls.js
+++ src/ekhn/schema/glaubensimpuls.js
@@ -1,12 +1,15 @@
 import cron from 'node-cron';
 import fetch from 'isomorphic-fetch';
+import get from 'lodash/get';
 
 // Url for Glaubensimpuls
-const url = 'https://api.instagram.com/v1/users/976930779/media/recent?access_token=15212360.ba4c844.05522f13a1e84fe5b8280d65c0cc8353&count=1';
+const url =
+  'https://api.instagram.com/v1/users/976930779/media/recent?access_token=15212360.ba4c844.05522f13a1e84fe5b8280d65c0cc8353&count=1';
 // authkey hier generieren lassen (andere Seiten gehen nicht (mehr)): http://instagramwordpress.rafsegat.com/docs/get-access-token/#access_token=15212360.ba4c844.f7f2feb22b774ade838666d3dd0affb5
 const testData = {
   pagination: {
-    next_url: 'https://api.instagram.com/v1/users/976930779/media/recent?access_token=15212360.ba4c844.f7f2feb22b774ade838666d3dd0affb5\u0026count=1\u0026max_id=1399824060582295380_976930779',
+    next_url:
+      'https://api.instagram.com/v1/users/976930779/media/recent?access_token=15212360.ba4c844.f7f2feb22b774ade838666d3dd0affb5\u0026count=1\u0026max_id=1399824060582295380_976930779',
     next_max_id: '1399824060582295380_976930779',
   },
   meta: {
@@ -15,14 +18,11 @@ const testData = {
   data: [
     {
       attribution: null,
-      tags: [
-        'adventszeit',
-        'glaubensimpuls',
-        'adventskalender',
-      ],
+      tags: ['adventszeit', 'glaubensimpuls', 'adventskalender'],
       user: {
         username: 'glaubensimpuls',
-        profile_picture: 'https://scontent.cdninstagram.com/t51.2885-19/14596804_145964162529557_8997841624809603072_a.jpg',
+        profile_picture:
+          'https://scontent.cdninstagram.com/t51.2885-19/14596804_145964162529557_8997841624809603072_a.jpg',
         id: '976930779',
         full_name: 'Glaubensimpuls',
       },
@@ -32,17 +32,20 @@ const testData = {
       filter: 'Normal',
       images: {
         low_resolution: {
-          url: 'https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
+          url:
+            'https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
           width: 320,
           height: 320,
         },
         thumbnail: {
-          url: 'https://scontent.cdninstagram.com/t51.2885-15/s150x150/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
+          url:
+            'https://scontent.cdninstagram.com/t51.2885-15/s150x150/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
           width: 150,
           height: 150,
         },
         standard_resolution: {
-          url: 'https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
+          url:
+            'https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/15276660_1868281953406201_7949528957844455424_n.jpg?ig_cache_key=MTM5OTgyNDA2MDU4MjI5NTM4MA%3D%3D.2',
           width: 640,
           height: 640,
         },
@@ -56,10 +59,12 @@ const testData = {
       users_in_photo: [],
       caption: {
         created_time: '1481092053',
-        text: '#Glaubensimpuls zum 7. Dezember #Adventskalender \nWir w\u00fcnschen Dir eine gesegnete #Adventszeit \nhier t\u00e4gl. als Video http://ow.ly/dLLU306RK3V',
+        text:
+          '#Glaubensimpuls zum 7. Dezember #Adventskalender \nWir w\u00fcnschen Dir eine gesegnete #Adventszeit \nhier t\u00e4gl. als Video http://ow.ly/dLLU306RK3V',
         from: {
           username: 'glaubensimpuls',
-          profile_picture: 'https://scontent.cdninstagram.com/t51.2885-19/14596804_145964162529557_8997841624809603072_a.jpg',
+          profile_picture:
+            'https://scontent.cdninstagram.com/t51.2885-19/14596804_145964162529557_8997841624809603072_a.jpg',
           id: '976930779',
           full_name: 'Glaubensimpuls',
         },
@@ -77,20 +82,23 @@ export default ({ schema }) => {
   let item = testData;
 
   // Get Losung
-  const get = () => {
+  const getLosung = () => {
     // Get Glaubensimpuls from url
-    fetch(url).then((res) => {
-      return res.json();
-    }).then((json) => {
-      item = json;
-      console.log('Neuer Impuls');
-    }).catch(err => console.log(err));
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        item = json;
+        console.log('Neuer Impuls');
+      })
+      .catch(err => console.log(err));
   };
 
-  cron.schedule('00 00 * * * *', get);
+  cron.schedule('00 00 * * * *', getLosung);
 
   // Get initial
-  get();
+  getLosung();
 
   if (!item || !item.data || !item.data[0]) item = testData;
 
@@ -99,32 +107,34 @@ export default ({ schema }) => {
     query: `
       glaubensimpuls: Glaubensimpuls
     `,
-    resolvers: { Query: {
-      glaubensimpuls: () => ({
-        thumb: {
-          url: item.data[0].images.thumbnail.url,
-          width: item.data[0].images.thumbnail.width,
-          height: item.data[0].images.thumbnail.height,
-          caption: item.data[0].caption ? item.data[0].caption.text : '',
-          source: item.data[0].user.profile_picture,
-        },
-        small: {
-          url: item.data[0].images.low_resolution.url,
-          width: item.data[0].images.low_resolution.width,
-          height: item.data[0].images.low_resolution.height,
-          caption: item.data[0].caption ? item.data[0].caption.text : '',
-          source: item.data[0].user.profile_picture,
-        },
-        large: {
-          url: item.data[0].images.standard_resolution.url,
-          width: item.data[0].images.standard_resolution.width,
-          height: item.data[0].images.standard_resolution.height,
-          caption: item.data[0].caption ? item.data[0].caption.text : '',
-          source: item.data[0].user.profile_picture,
-        },
-        tags: item.data[0].tags,
-      }),
-    } },
+    resolvers: {
+      Query: {
+        glaubensimpuls: () => ({
+          thumb: {
+            url: get(item, 'data.0.images.thumbnail.url'),
+            width: get(item, 'data.0.images.thumbnail.width'),
+            height: get(item, 'data.0.images.thumbnail.height'),
+            caption: get(item, 'data.0.caption.text'),
+            source: get(item, 'data.0.user.profile_picture'),
+          },
+          small: {
+            url: get(item, 'data.0.images.low_resolution.url'),
+            width: get(item, 'data.0.images.low_resolution.width'),
+            height: get(item, 'data.0.images.low_resolution.height'),
+            caption: get(item, 'data.0.caption.text'),
+            source: get(item, 'data.0.user.profile_picture'),
+          },
+          large: {
+            url: get(item, 'data.0.images.standard_resolution.url'),
+            width: get(item, 'data.0.images.standard_resolution.width'),
+            height: get(item, 'data.0.images.standard_resolution.height'),
+            caption: get(item, 'data.0.caption.text'),
+            source: get(item, 'data.0.user.profile_picture'),
+          },
+          tags: get(item, 'data.0.tags', []),
+        }),
+      },
+    },
     schema: `
       type Glaubensimpuls {
         thumb: Image
