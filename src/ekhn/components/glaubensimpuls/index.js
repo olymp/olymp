39b5f3eb7c81f2826538989bd8eb847a491ae05a
Philipp Achenbach <github@philipp-achenbach.de>
2019-01-28 18:00:26 +0100

    glaubensimpuls

--- src/ekhn/components/glaubensimpuls/index.js
+++ src/ekhn/components/glaubensimpuls/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { graphql, gql, Image } from 'olymp';
-import { DataLoader } from 'olymp/cms-core';
-import Tags from '../../components/tags';
+import React, { Component } from "react";
+// import { graphql, gql, Image } from "olymp";
+// import { DataLoader } from "olymp/cms-core";
+import Iframe from "react-iframe";
+// import Tags from "../../components/tags";
 
-@graphql(gql`
+/* @graphql(gql`
   query glaubensimpuls {
     glaubensimpuls: glaubensimpuls {
       thumb {
@@ -30,15 +31,30 @@ import Tags from '../../components/tags';
       tags
     }
   }
-`)
+`) */
 export default class GlaubensimpulsBlock extends Component {
   render() {
-    const { data, style, className, isBlock } = this.props;
-    const { tags } = data.glaubensimpuls || {};
+    const { /* data, */ style = {}, className /*, isBlock */ } = this.props;
+    /* const { tags } = data.glaubensimpuls || {};
     const bild = (data.glaubensimpuls || {}).small;
-    const { caption } = (bild || {});
+    const { caption } = (bild || {}); */
 
     return (
+      <div
+        className={className}
+        style={{ ...style, marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <Iframe
+          url="https://apps.02.cmsrv.de/ekhn/responsive.html"
+          width="100%%"
+          height="450px"
+          allowFullScreen
+          position="relative"
+        />
+      </div>
+    );
+
+    /* return (
       <DataLoader className={className} style={style} isEmpty={data.glaubensimpuls} placeholder="Kein Glaubensimpuls vorhanden" loading="Glaubensimpuls wird geladen">
         <Image value={bild} lightbox width="100%" className="block" />
         {!isBlock && (
@@ -48,6 +64,6 @@ export default class GlaubensimpulsBlock extends Component {
           </div>
         )}
       </DataLoader>
-    );
+    ); */
   }
 }
